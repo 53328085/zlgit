260c084e290a035deced4483f590834ba574b6e2
zzl <zzl@2021>
2022-08-11 16:16:54 +0800

    redux 持久化

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client';
 import { Provider } from 'react-redux'
+import {PersistGate} from 'redux-persist/integration/react'
+import {persistStore} from 'redux-persist'
 import store from './redux/store'
 import {ConfigProvider} from 'antd'
 import 'antd/dist/antd.variable.min.css'
 import './assets/css/index.less'
 import App from './app'
 import antdconfig from './antdconfig';
-
+let persistor = persistStore(store)
 const root = createRoot(document.getElementById('root'));
 
 
 root.render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <ConfigProvider {...antdconfig}>
       <App />
     </ConfigProvider>
+    </PersistGate>
   </Provider>
 
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,34 @@
 import {configureStore} from '@reduxjs/toolkit'
+import {combineReducers} from 'redux'
+import thunk from 'redux-thunk'
+import {persistStore, persistReducer} from 'redux-persist' // redux 持久化
+import storage from 'redux-persist/lib/storage' // redux 持久化
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user' 
 import system from './systemconfig' //根据IP或域名获取系统配置
 import theme from './them' // 配置 antd 
+
+const reducers = combineReducers({
+  user,
+  system,
+  theme
+})
+const persistConfig = {
+  key: 'redux_state',
+  storage
+}
+const persistedReducer = persistReducer(persistConfig, reducers);
 export default configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk]
+})
+
+/* export default configureStore({
    reducer: {
     user,
     system,
     theme
    },
   // middleware:[composeWithDevTools] 引用‘composeWithDevTools’导致异步action出错
-})
+}) */
