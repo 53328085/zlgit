0ff0ef5396c6b0caf499b356df9232c53f262d44
zzl <zzl@2021>
2022-09-09 09:57:29 +0800

    title布局

--- src/components/titlelayout.js
+++ src/components/titlelayout.js
@@ -5,7 +5,7 @@ const Boxdiv = styled.div`
   border-radius: 4px;
   border: 1px solid #d7d7d7;
   display: grid;
-  grid-template-rows: 32px 1fr;
+  grid-template-rows: ${(props) => props.title ? `32px 1fr` : '1fr'};
   flex:1;
   background-color: #fff;
   h4 {
@@ -17,8 +17,8 @@ const Boxdiv = styled.div`
 `
 export default function Titlelayout({title='', children}) {
   return (
-    <Boxdiv>
-        <h4>{title}</h4>
+    <Boxdiv title={title}>
+        {title ? (<h4>{title}</h4>) : null}
         {children}
     </Boxdiv>
   )
