ae1a2cf6fc2c88b8401499d1f093620ead31b508
ybmq <ybmq@2021>
2022-09-09 16:41:15 +0800

    告警监控

--- src/pages/monitoring/warning/card.jsx
+++ src/pages/monitoring/warning/card.jsx
@@ -4,21 +4,21 @@ import styled from 'styled-components'
 export default function Card(props) {
 
   return (
-    <div stye={{
-        dispaly:'flex',
+    <div style={{
+        display:'flex',
         alignItems:'center',
         padding:'0 16px',
         width:'320px',
-        height:'64px'
+        height:'64px',
+        backgroundColor:'#FFFFFF',
+        margin:'16px 16px 16px 0',
+        border:'1px solid #d7d7d7',
+        borderRadius:'4px',
+        fontSize:16
         }}>
         <img src={props.imgurl} alt="" />
-        <div>{props.title}</div>
-        <div>{props.value}</div>
+        <div style={{marginLeft:'25px'}}>{props.title}</div>
+        <div style={{fontSize:18,margin:'0 0 0 auto'}}>{props.value}</div>
     </div>
-    // <div>
-    //      <img src={props.imgurl} alt="" />
-    //     <div>{props.title}</div>
-    //     <div>{props.value}</div>
-    // </div>
   )
 }
