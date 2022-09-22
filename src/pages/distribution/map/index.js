bca4140a8423d775d20b47d60dc1cf55eb60ed33
ybmq <ybmq@2021>
2022-09-22 16:50:22 +0800

    变压器监测

--- src/pages/distribution/map/index.js
+++ src/pages/distribution/map/index.js
@@ -1,7 +1,21 @@
 import React from 'react'
-
+import { Select } from 'antd'
+import style from './style.module.less'
+import peidian from '@imgs/peidian.png'
+const { Option } = Select;
 export default function Index() {
   return (
-    <div>配电系统图</div>
+    <div className={style.map}>
+      <div className={style.mapheader}>
+        <span className={style.areachoose}>区域选择</span>
+        <Select defaultValue="" style={{ width: 330 }} size="default">
+          <Option value="1">Jack</Option>
+          <Option value="2">Lucy</Option>
+        </Select>
+      </div>
+      <div style={{marginTop: 16}}>
+        <img src={peidian}></img>
+      </div>
+    </div>
   )
 }
