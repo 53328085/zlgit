834aa920cc0d74b009d086a606f11757cf929654
ybgsh <ybgsh@chint.com>
2022-09-27 10:52:27 +0800

    运行监控

--- src/pages/photovoltaic/monitor/index.js
+++ src/pages/photovoltaic/monitor/index.js
@@ -3,9 +3,32 @@ import style from './style.module.less'
 import { Select } from 'antd'
 import mapImg from './map.png'
 import PVImg from './PVImg.png'
+import inverter from './inverter.png'
 
 export default function Index() {
   const {Option} = Select;
+
+  const items = [
+    {
+      State:'Normal',
+      Power:320,
+    },{
+      State:'Normal',
+      Power:320,
+    },{
+      State:'Normal',
+      Power:320,
+    },{
+      State:'Normal',
+      Power:320,
+    },{
+      State:'Normal',
+      Power:320,
+    },{
+      State:'Normal',
+      Power:320,
+    },
+  ]
 
   const DataItem = (item)=>{
     return <div className={style.dataItem}>
@@ -42,7 +65,50 @@ export default function Index() {
             {DataItem({name:'电流',value:15.21, unit:'A'})}
           </div>
         </div>
-        <div className={style.bottomLeft}></div>
+        <div className={style.bottomLeft}>
+          <div>电站常规参数</div>
+          <div className={style.bottomLeftContent}>
+            <div className={style.contentValue}>
+              <span className={style.ValueTitle}>电站功率</span>
+              <span className={style.ValueData}>567890323</span>
+              <span className={style.ValueUnit}>kW</span>
+            </div>
+            <div className={style.contentValue}>
+              <span className={style.ValueTitle}>当天发电量</span>
+              <span className={style.ValueData}>8965.25</span>
+              <span className={style.ValueUnit}>kW</span>
+            </div>
+          </div>
+          <div className={style.bottomLeftContent}>
+            <div className={style.contentValue}>
+              <span className={style.ValueTitle}>累计发电量</span>
+              <span className={style.ValueData}>1352545.25</span>
+              <span className={style.ValueUnit}>kW</span>
+            </div>
+            <div className={style.contentValue}>
+              <span className={style.ValueTitle}>电站装机容量</span>
+              <span className={style.ValueData}>3.025</span>
+              <span className={style.ValueUnit}>MWp</span>
+            </div>
+          </div>
+        </div>
+        <div className={style.bottomRight}>
+          {items.map((value, index) => {
+            return(<div className={style.card} key={index}>
+              <img src={inverter} className={style.inverterImg}></img>
+              <div>
+                <div className={style.cardContent}>
+                  <div className={style.cardTitle}>1# 逆变器:</div>
+                  <div className={style.cardValue}>{value.State == 'Normal' ? '正常' : ''}</div>
+                </div>
+                <div className={style.cardContent} style={{marginTop:16}}>
+                  <div className={style.cardTitle}>功率:</div>
+                  <div className={style.cardValue}>{value.Power} kW</div>
+                </div>
+              </div>
+            </div>)
+          })}
+        </div>
       </div>
     </div>
   )
---
+++ src/pages/photovoltaic/monitor/inverterDetail.js
@@ -0,0 +1,7 @@
+import React from "react";
+import style from './style.module.less';
+
+export default function Index(){
+
+    
+}
