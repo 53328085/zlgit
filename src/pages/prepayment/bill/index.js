e00891ab6e965e4e2fe6559b762edd6424c9e34a
ybgsh <ybgsh@chint.com>
2022-12-21 13:11:06 +0800

    camera调整

--- src/pages/prepayment/bill/index.js
+++ src/pages/prepayment/bill/index.js
@@ -1,57 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './style.module.less'
-import { Select, Tabs } from 'antd'
-import styled from 'styled-components'
+import { Select } from 'antd'
 import Account from './account'
 import Refund from './refund'
 
-const Tabsbox = styled(Tabs)`
-  .ant-tabs-nav {
-    margin-bottom: 0px;
-   .ant-tabs-nav-list {
-    .ant-tabs-tab {
-        border-radius: 4px 4px 0 0;
-        height: 40px;
-        width: 144px;
-        justify-content: center;
-        font-size: 14px;
-        background-color: #fff;      
-        &:hover {
-            background-color: var(--ant-primary-color);
-            color: #fff;
-        }
-    }
-    .ant-tabs-tab-active {
-        background-color: var(--ant-primary-color);
-       
-        .ant-tabs-tab-btn {
-            color:#fff;
-            transition: color 100ms;
-        }
-    }
-   }  
- 
-}
-`
+import CustContext from '@com/content.js'
+import Pagecount from '@com/pagecontent'
+
 
 export default function Index() {
   const { Option } = Select;
 
-  const account = ()=>{
-    return <Account></Account>
-  }
-  const refund = ()=>{
-    return <Refund></Refund>
-  }
+  const [value, setvalue] = useState('account')
 
-  const items = [
-    { label: '账户交易', key: 'account', children: account() },
-    { label: '退费处理', key: 'refund', children: refund() },
+  const tabs = [
+    { label: '账户交易', key: 'account' },
+    { label: '退费处理', key: 'refund' },
   ]
-
-  const onChange = (checkedValues) => {
-    console.log('checked = ', checkedValues);
-  };
+  const propsData ={
+    tabs,
+    value,
+    setvalue
+  }
 
   return (
     <div>
@@ -65,9 +35,13 @@ export default function Index() {
           <Option value="1">正泰物联项目</Option>
         </Select>
       </div>
-      <div className={style.content}>
-      <Tabsbox onChange={onChange} animated tabBarGutter={16} type="card" items={items}></Tabsbox>
-      </div>
+      <CustContext.Provider value={propsData}>
+      <Pagecount showserach={false}>   
+        {
+          value == 'account' ? <Account /> : <Refund />
+        }
+      </Pagecount>
+    </CustContext.Provider>
     </div>
   )
 }
