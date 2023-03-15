0c9cb12cd5a65b9d5fdcb2c2aa6ec4ac78409fe1
53328085 <53328085@qq.com>
2023-03-15 14:00:55 +0800

    区域管理接口变动修改

--- src/pages/test/fform.js
+++ src/pages/test/fform.js
@@ -1,42 +1,122 @@
-import { Cascader } from 'antd';
-const options = [
-  {
-    value: 'zhejiang',
-    label: 'Zhejiang',
-    children: [
-      {
-        value: 'hangzhou',
-        label: 'Hangzhou',
-        children: [
-          {
-            value: 'xihu',
-            label: 'West Lake',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    value: 'jiangsu',
-    label: 'Jiangsu',
-    children: [
-      {
-        value: 'nanjing',
-        label: 'Nanjing',
-        children: [
-          {
-            value: 'zhonghuamen',
-            label: 'Zhong Hua Men',
-          },
-        ],
-      },
-    ],
-  },
-];
-const onChange = (value) => {
-  console.log(value);
+import { Button, Form, Input, Select, Space, Tooltip, Typography } from 'antd';
+const { Option } = Select;
+const onFinish = (values) => {
+  console.log('Received values of form: ', values);
 };
 const App = () => (
-  <Cascader defaultValue={['zhejiang', 'hangzhou', 'xihu']} options={options} onChange={onChange} />
+  <Form
+    name="complex-form"
+    onFinish={onFinish}
+    labelCol={{
+      span: 8,
+    }}
+    wrapperCol={{
+      span: 16,
+    }}
+    style={{
+      maxWidth: 600,
+    }}
+  >
+    <Form.Item label="Username">
+      <Space>
+        <Form.Item
+          name="username"
+          noStyle
+          rules={[
+            {
+              required: true,
+              message: 'Username is required',
+            },
+          ]}
+        >
+          <Input
+            style={{
+              width: 160,
+            }}
+            placeholder="Please input"
+          />
+        </Form.Item>
+        <Tooltip title="Useful information">
+          <Typography.Link href="#API">Need Help?</Typography.Link>
+        </Tooltip>
+      </Space>
+    </Form.Item>
+    <Form.Item label="Address">
+      <Input.Group compact>
+        <Form.Item
+          name={['address', 'province']}
+          noStyle
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Select placeholder="Select province">
+            <Option value="Zhejiang">Zhejiang</Option>
+            <Option value="Jiangsu">Jiangsu</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          name={['address', 'street']}
+          noStyle
+          rules={[
+            {
+              required: true,
+              message: 'Street is required',
+            },
+          ]}
+        >
+          <Input
+            style={{
+              width: '50%',
+            }}
+            placeholder="Input street"
+          />
+        </Form.Item>
+      </Input.Group>
+    </Form.Item>
+    <Form.Item
+      label="BirthDate"
+      style={{
+        marginBottom: 0,
+      }}
+    >
+      <Form.Item
+        name="year"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+        style={{
+          display: 'inline-block',
+          width: 'calc(50% - 8px)',
+        }}
+      >
+        <Input placeholder="Input birth year" />
+      </Form.Item>
+      <Form.Item
+        name="month"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+        style={{
+          display: 'inline-block',
+          width: 'calc(50% - 8px)',
+          margin: '0 8px',
+        }}
+      >
+        <Input placeholder="Input birth month" />
+      </Form.Item>
+    </Form.Item>
+    <Form.Item label=" " colon={false}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
 );
 export default App;
