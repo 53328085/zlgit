27dfad19bc497d2cc83ce8776b44b746ec4b6ec7
53328085 <53328085@qq.com>
2023-03-17 14:59:03 +0800

    储能管理路由

--- src/router/storage.js
+++ src/router/storage.js
@@ -1,11 +1,27 @@
-/*  碳排管理 */
+/*  储能管理 */
 import {lazy} from 'react'
 import store from '@redux/store'
-const Monitor = lazy(() => import("@pages/storage/monitor"))
+const Summary = lazy(() => import("@pages/storage/summary"))
+const AlternatorMonitor = lazy(() => import("@pages/storage/alternatorMonitor"))
+const StorageMonitor = lazy(() => import("@pages/storage/storageMonitor"))
+const StorageControl = lazy(() => import("@pages/storage/storageControl"))
+const ConsumeStatistics = lazy(() => import("@pages/storage/consumeStatistics"))
+const ReportStatistics = lazy(() => import("@pages/storage/reportStatistics"))
+const AlarmMessage = lazy(() => import("@pages/storage/alarmMessage"))
+const OperationLog = lazy(() => import("@pages/storage/operationLog"))
+const StroageReport = lazy(() => import("@pages/storage/StroageReport"))
 
 const menus = [];
 const components = {
-    '011101': Monitor, 
+    '011101': Summary, 
+    '011102': AlternatorMonitor, 
+    '011103': StorageMonitor, 
+    '011104': StorageControl, 
+    '011105': ConsumeStatistics, 
+    '011106': ReportStatistics, 
+    '011107': AlarmMessage, 
+    '011108': OperationLog, 
+    '011109': StroageReport, 
 }
 store.subscribe(() => {
     const runmen= store.getState().system.menus.siderRunMenus?.['runtimeStorage']
