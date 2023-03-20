686953cddea913e9a856143ceb2882cfaa8be89c
53328085 <53328085@qq.com>
2023-03-20 09:27:17 +0800

    储能--储能控制

--- src/pages/storage/reportStatistics/index.jsx
+++ src/pages/storage/reportStatistics/index.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
-
-export default function Index() {
+import React, {useState} from 'react'
+import CustContext from '@com/content.js'
+import Pagecount from '@com/pagecontent'
+import CModal from '@com/useModal'
+import {useSelector} from 'react-redux'
+import {selectProjectId} from '@redux/systemconfig.js'
+export default function Index() {  
+  const projectId = useSelector(selectProjectId)
+  const coms = {
+   Manual: Manual,
+   Automate: Automate,  
+  }
+  const ProjectCom = coms[value] || Manual
   return (
-    <div>报表统计</div>
+    <CustContext.Provider>
+    <Pagecount showserach={true}>   
+        <ProjectCom projectId={projectId} CModal={CModal} />
+    </Pagecount>
+    </CustContext.Provider>
   )
 }
