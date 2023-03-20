97b0bd47ca07e59fcbe3c2aab5fbf79819c05e71
ybmq <ybmq@2021>
2023-03-20 13:19:06 +0800

    commit

--- src/pages/electric/warning/columns.jsx
+++ src/pages/electric/warning/columns.jsx
@@ -1,46 +1,42 @@
 import {Space} from 'antd'
 export const  columns = [
     {
-        title: '最新报告时间',
+        title: '最新告警时间',
         dataIndex: 'name',
         key: 'name',
       },
       {
-        title: '最新告警时间',
+        title: '最新告警事件',
         dataIndex: 'alarmTime',
         key: 'alarmTime',
       },
       {
-        title: '所属项目',
+        title: '告警等级',
         dataIndex: 'customer',
         key: 'customer',
       },
       {
-        title: '所属区域/建筑/楼层',
+        title: '设备编号',
         dataIndex: 'place',
         key: 'place',
       },
       {
-        title: '安装地址',
+        title: '设备类型',
         dataIndex: 'address',
         key: 'address',
       },
       {
-        title: '设备类型',
+        title: '安装地址',
         dataIndex: 'category',
         key: 'category',
       },
+ 
       {
-        title: '设备编号',
-        dataIndex: 'sn',
-        key: 'sn',
-      },
-      {
-        title: '操作',
+        title: '设备信息',
         key: 'option',
         align:'center',
         render:(_,val)=>(
-            <a style={{color:'red',textDecoration:'underline'}}>派单</a>
+            <a style={{color:'red',textDecoration:'underline'}}>设备信息</a>
         ),
       },
       {
