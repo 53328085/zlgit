94fe41771ab6db715f467e99b0902b19983e4983
53328085 <53328085@qq.com>
2023-03-22 13:19:42 +0800

    储能路由增加

--- src/router/storage.js
+++ src/router/storage.js
@@ -11,17 +11,22 @@ const AlarmMessage = lazy(() => import("@pages/storage/alarmMessage"))
 const OperationLog = lazy(() => import("@pages/storage/operationLog"))
 const StroageReport = lazy(() => import("@pages/storage/StroageReport"))
 
+
+const ChargingOrder = lazy(() => import("@pages/storage/chargingOrder"))
+const Environment = lazy(() => import("@pages/storage/environment"))
 const menus = [];
 const components = {
-    '011101': Summary, 
-    '011102': AlternatorMonitor, 
+    '011101': Summary,  //储能概述
+    '011102': AlternatorMonitor, // 
     '011103': StorageMonitor, 
     '011104': StorageControl, 
     '011105': ConsumeStatistics, 
     '011106': ReportStatistics, 
-    '011107': AlarmMessage, 
-    '011108': OperationLog, 
-    '011109': StroageReport, 
+    '011107': ChargingOrder, 
+    '011108': Environment, 
+    '011109': AlarmMessage, 
+    '011110': OperationLog, 
+    '011111': StroageReport, 
 }
 store.subscribe(() => {
     const runmen= store.getState().system.menus.siderRunMenus?.['runtimeStorage']
