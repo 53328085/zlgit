6b4dbb19db24366a7897decb52f3fdb33d063184
ybgsh <ybgsh@chint.com>
2023-03-25 15:26:35 +0800

    储能路由

--- src/router/designer/storage.js
+++ src/router/designer/storage.js
@@ -1,13 +1,18 @@
 /*  储能 配置 */
 import {lazy} from 'react'
 import store from '@redux/store'
-const Base = lazy(() => import("@pages/storage/configure/base"))
-const Quota = lazy(() => import("@pages/storage/configure/quota")) 
+const Base = lazy(() => import("@pages/storage/configure/price"))
+const Strategy = lazy(() => import("@pages/storage/configure/strategy")) 
+const StorageDevice = lazy(() => import("@pages/storage/configure/storageDevice")) 
+const StorageEnvironment = lazy(() => import("@pages/storage/configure/storageEnvironment")) 
+const StorageSetting = lazy(() => import("@pages/storage/configure/storageSetting")) 
 const menus = [];
 const components = {
     '020901': Base,
-    '021001': Quota,
-    
+    '020902': Strategy,
+    '020903': StorageDevice,
+    '020904': StorageEnvironment,
+    '020905': StorageSetting
 }
 store.subscribe(() => {
     const runmen= store.getState().system.menus?.siderDesignerMenus?.['storage'] 
