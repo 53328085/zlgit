1627f75c82f43fdf2501880d3660a40272d12fba
53328085 <53328085@qq.com>
2023-04-06 08:59:45 +0800

    设置控制

--- src/pages/storage/configure/manualMode/power.jsx
+++ src/pages/storage/configure/manualMode/power.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from 'react'
 import styled from 'styled-components'
 import {Typography, Image, Form, Space, Button, Input, message, InputNumber, Select, Switch} from 'antd'
 import {CheckCircleFilled } from '@ant-design/icons'
+import {useRequest} from 'ahooks'
 import {StorageParameterSetupDesigner} from '@api/api'
 import {custMsg}  from '@com/usehandler'
 import Titlelayout from '@com/titlelayout'
@@ -54,31 +55,44 @@ justify-content: center;
 export default function Manual({projectId,  areaId, CModal}) {
  
   // UpdateSiteOnOffGrid
- 
+  const queryruntimesetting = () => {
+    return StorageParameterSetupDesigner.QuerySetup(projectId, areaId).then(res => {
+         let {success, data} = res
+         if (success && data) {
+            return data
+         }else {
+            return {}
+         }
+    }).catch(e => {
+         return e
+    })
+  }
+
   const pid = useRef()
-  const  QueryRuntimeSetting= async () => {
-       try {
-        let {success, data} = await StorageParameterSetupDesigner.QuerySetup(projectId, areaId)
-       if (success) {
-        let {id, ...init} = data
+ const {loading, run} = useRequest(queryruntimesetting, {
+    manual: true,
+    onSuccess: (data) => {
+        let {id, antiReflux, demandControl, ...init} = data
         pid.current = id
         
         form.setFieldsValue({
+            antiReflux: antiReflux == 1,
+            demandControl: demandControl == 1,
              ...init
          })  
-       }
-       
-       } catch (error) {
-        console.log(error)
-       }
-       
-  }    
+    },
+    onError: (e) => {
+        console.log(e)
+    }
+ })
+
+
   const [form] = Form.useForm()
   const rref = useRef()
  
   const onClose = () => {
     rref.current.onCancel();
-    QueryRuntimeSetting() 
+    run() 
   } 
   const  Updatedata = async () => {
    try {
@@ -113,13 +127,13 @@ export default function Manual({projectId,  areaId, CModal}) {
 
   }, [projectId, areaId])
   return (
-    <Titlelayout title={<div style={{display: 'flex', justifyContent: 'space-between'}}><span>参数设置</span><Space size={16}><CustButton type='primary' onClick={QueryRuntimeSetting}>读取</CustButton><CustButton type='primary'>设置</CustButton></Space></div>}>
+    <Titlelayout title={<div style={{display: 'flex', justifyContent: 'space-between'}}><span>参数设置</span><Space size={16}><CustButton type='primary' onClick={run} loading={loading}>读取</CustButton><CustButton type='primary'>设置</CustButton></Space></div>}>
         <Mainbox>
              
             <Formbox layout="inline" form={form}  >
                     <div className='items'>
-                        <Item label="储能容量" name="capacity">
-                            <Input addonAfter="kw" disabled style={{width: '168px'}} /> 
+                        <Item label="储能容量" name="capacity">  
+                            <InputNumber  step="0.01" min={0.01} precision={2} addonAfter="kw" style={{width: '168px'}}  /> 
                         </Item>
                         <Item label="温度上限" name="tempUpperLimit"   rules={[
                             {
@@ -129,14 +143,14 @@ export default function Manual({projectId,  areaId, CModal}) {
                           <InputNumber  step="0.01" min={0.01} precision={2} addonAfter="℃" style={{width: '168px'}}  /> 
                         </Item> 
                         <Item label="SOC上限" name="socUpperLimit">
-                            <Input addonAfter="%" disabled style={{width: '168px'}} /> 
+                           <InputNumber min={1} max={100}  step="0.01"  precision={2} addonAfter="%" style={{width: '168px'}}  /> 
                         </Item>
                         <Item label="SOC下限" name="socLowerLimit"   rules={[
                             {
                                 required: true, 
                             }
                         ]}>
-                        <InputNumber  step="0.01" min={0.01} precision={2} addonAfter="%" style={{width: '168px'}}  /> 
+                        <InputNumber  step="0.01" min={1} max={100} precision={2} addonAfter="%" style={{width: '168px'}}  /> 
                         </Item>
                         <Item name='chargeMode' label="充电模式">
                             <Select options={[
@@ -177,17 +191,18 @@ export default function Manual({projectId,  areaId, CModal}) {
                     </div>   
                     <div className='items'>
                     <Item label="变压器最大需量" name="transformMaxDemand">
-                            <Input addonAfter="kw" disabled style={{width: '168px'}} /> 
+                          
+                            <InputNumber  step="0.01" min={0.01} precision={2} addonAfter="kw" style={{width: '168px'}}  /> 
                         </Item>
                         <Item label="需量上限系数" name="demandLimitCoefficient"   rules={[
                             {
                                 required: true, 
                             }
                         ]}>
-                          <InputNumber  step="0.01" min={0.01} precision={2} addonAfter="%" style={{width: '168px'}}  /> 
+                            <InputNumber  step="0.01" min={1} max={100} precision={2} addonAfter="%" style={{width: '168px'}}  /> 
                         </Item> 
                         <Item label="电网供电功率下限" name="demandLimitCoefficient">
-                            <Input addonAfter="kw" disabled style={{width: '168px'}} /> 
+                        <InputNumber  step="0.01" min={0.01} precision={2} addonAfter="kw" style={{width: '168px'}}  /> 
                         </Item>
                         <Item label="电网供电功率下限误差" name="demandLimitCoefficient"   rules={[
                             {
