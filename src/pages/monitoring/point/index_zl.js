ebce4f76655e10cc452982833e81e76b1793b183
53328085 <53328085@qq.com>
2023-04-12 19:30:00 +0800

    优化修改

--- src/pages/monitoring/point/index_zl.js
+++ src/pages/monitoring/point/index_zl.js
@@ -4,15 +4,15 @@ import UseHeader from '@com/useHeader'
 import { Input, Button, Select, Radio, Pagination, FormTable, message } from 'antd'
 import { SearchOutlined } from '@ant-design/icons';
 import { Link, useNavigate, useLocation } from 'react-router-dom'
-import { useRequest, useAntdTable} from "ahooks";
+import { useRequest } from "ahooks";
 import style from './style.module.less'
 import Icard from './card'
 import imgurl from './images/index.js'
 import { Monitoring } from '@api/api.js'
 import { selectProjectId, selectOneLevel, selectOneLevelDefaultId, levelDefaultLabel } from '@redux/systemconfig.js'
-
+import { SemanticClassificationFormat } from 'typescript';
 import Table from '@com/useTable'
-
+import { area } from '@antv/g2plot';
 
 
 export default function Index(props) {
@@ -29,8 +29,8 @@ export default function Index(props) {
 
   const defaultLabel = useSelector(levelDefaultLabel)
 
-  //const [defaultArea, setDefaultArea] = useState(areaList[0] ? areaList[0].id : undefined)
-  let [areaId, setAreaId] = useState(LevelDefaultId)
+  const [defaultArea, setDefaultArea] = useState(areaList[0] ? areaList[0].id : undefined)
+  let [areaId, setAreaId] = useState(defaultArea)
   let [optionsGateway, setoptionsGateway] = useState([])
   const [changeTag, setChangeTag] = useState('')
   const [isCard, setisCard] = useState(true)//卡片模式true或列表模式false
@@ -132,59 +132,6 @@ export default function Index(props) {
     })
   }
 
-/* alike
-: 
-""
-areaId
-: 
-1
-category
-: 
-""
-deviceStyle
-: 
-1
-gatewayId
-: 
-0
-pageNum
-: 
-1
-pageSize
-: 
-12
-projectId
-: 
-45
-state
-: 
-0 */
-
-  let defaultparams = {
-    projectId,
-    areaId: areaId,
-    gatewayId: 0,
-    deviceStyle: deviceStyle,
-    category: '',
-    alike: '',
-    state: 0,
-  }
-  const {tableProps, search} = useAntdTable(getOverviewData, {
-  //  form,
-    defaultParams: [
-     {current: 1, pageSize: 15},
-     defaultparams,
-    ],
-    refreshDeps: [areaId, deviceStyle]
-    
-  })
- const {submit} = search
-
-
-
-
-
-
   // let [imgUrl, setimgUrl] = useState()
   const getGatewayImages = () => {//网关图片
     return CategoryImages({ projectId: projectId, group: overView.categories }).then(res => {
@@ -279,8 +226,8 @@ state
         <Select
           placeholder="请选择园区"
           size="middle"
-          key={LevelDefaultId}
-          defaultValue={LevelDefaultId}
+          key={defaultArea}
+          defaultValue={defaultArea}
           style={{ width: "200px" }}
           onChange={changeArea}
         >
--- src/pages/test/fform.js
+++ src/pages/test/fform.js
@@ -1,20 +1,38 @@
-import React, {useState} from 'react'
+import React, {useRef, useEffect} from 'react'
+import { drawEcharts } from "@com/useEcharts";
+export default function Index() {
+  const ref = useRef()
+  const option = {
+    legend: {
+      data: ['2015', '2016', '2017']
+    },
+    tooltip: {},
+    dataset: {
+     
+      dimensions: ['product', '2015', '2016', '2017'], // 维度
+      source: [
+        { product: 'Matcha Latte', '2015': 43.3, '2016': 85.8, '2017': 93.7 },
+        { product: 'Milk Tea', '2015': 83.1, '2016': 73.4, '2017': 55.1 },
+        { product: 'Cheese Cocoa', '2015': 86.4, '2016': 65.2, '2017': 82.5 },
+        { product: 'Walnut Brownie', '2015': 72.4, '2016': 53.9, '2017': 39.1 }
+      ]
+    },
+    // 声明一个 X 轴，类目轴（category）。默认情况下，类目轴对应到 dataset 第一列。
+    xAxis: { type: 'category' },
+    // 声明一个 Y 轴，数值轴。
+    yAxis: {},
+    // 声明多个 bar 系列，默认情况下，每个系列会自动对应到 dataset 的每一列。
+    series: [{ type: 'bar' }, { type: 'bar' }, { type: 'line' }]
+  };
+  useEffect(() => {
+    drawEcharts(ref.current, {...option})
 
-export default function fform() {
-  const [count, setCount] = useState({name: 'zl', age: 10});
-  const change = () => {
-    let obj = {name: 'zl'+Math.random(), age: Math.random()}
-    console.log(obj)
-    setCount({...obj})
-  }
-  const log = async () => {
-    console.log(count)
-  }
+  })
   return (
     <div>
-      <h5>{count.toString()}</h5>
-      <button onClick={change}>count</button>
-      <button onClick={log}>log</button>
+       
+       <div style={{width: '600px', height: '500px'}} ref={ref}></div>
+
     </div>
   )
 }
