e99099546a47e001ca9359c4cfc6b3be6f320e32
53328085 <53328085@qq.com>
2023-04-21 14:29:51 +0800

    错误页面，404页面优化, bug修复

--- src/router/notfound/index.js
+++ src/router/notfound/index.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import Pagecount from '@com/pagecontent'
+import React from 'react' 
 import notfound from './404.png'
-import {Image} from 'antd'
+import {Typography, Empty} from 'antd' 
 export default function Index() {
- 
+  const {Link, Text, Paragraph} = Typography
+  const style = {
+     flex: 1,
+     display: 'flex',
+     alignItems: 'center',
+     justifyContent: 'center',
+     flexDirection: 'column',
+  }
   return (
-    <div style={{display: 'flex', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-       <Image src={notfound} preview={false} />
-    </div>
+    <Empty image={notfound}
+         imageStyle={{width: '200px', height: '180px'}}
+         style={style}
+         description={<Paragraph><Text strong type="warning">抱歉！页面没找到</Text><Link  onClick={() => window.location.reload(true)}>刷新页面</Link>或<Link type='primary' ghost onClick={() => window.history.back()}>返回上一页</Link></Paragraph>}
+      />
   )
 }
