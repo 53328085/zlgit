81ff4a00fcf42c2fe10bd7eb8bdcb6aae8a8797f
ybgsh <ybgsh@chint.com>
2023-04-25 08:33:51 +0800

    调整

--- src/axios/api.js
+++ src/axios/api.js
@@ -1656,7 +1656,7 @@ export class AlarmManagement {
 //储能--环境监控
 export class StorageEnvironmentRuntime {
   static queryEnvironmentInfo = (projectId, areaId, siteId) => server.get(`/Storage/StorageEnvironmentRuntime/QueryEnvironmentInfo?projectId=${projectId}&areaId=${areaId}&siteId=${siteId}`)
-  static queryTrends = (projectId, storageRoomId, date) => server.get(`/Storage/StorageEnvironmentRuntime/QueryTrends?projectId=${projectId}&stroageRoomId=${storageRoomId}&date=${date}`)
+  static queryTrends = (projectId, storageRoomId, date) => server.get(`/Storage/StorageEnvironmentRuntime/QueryTrends?projectId=${projectId}&storageRoomId=${storageRoomId}&date=${date}`)
 }
 
 //储能--收益统计
