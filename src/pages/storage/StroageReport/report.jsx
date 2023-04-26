cb67eae6844728e91706cc610d81cc32b5ef5184
53328085 <53328085@qq.com>
2023-04-26 09:18:26 +0800

    修改

--- src/pages/storage/StroageReport/report.jsx
+++ src/pages/storage/StroageReport/report.jsx
@@ -216,67 +216,80 @@ const Ccontent = styled.div`
     source: [],
 })
 
+// 导出
+const downloadReport = () => {
+  //先生成图片再导出
+  html2canvas(document.getElementById('printRef'), {
+    // 导出pdf清晰度
+    allowTaint: true,
+    taintTest: false,
+    scale: '1',//设置放大倍数
+    height: document.getElementById('printRef').scrollHeight,
+    windowHeight: document.getElementById('printRef').scrollHeight,
+    // dpi: '192',
+    background: '#fff',
+    // 开启跨域配置
+    useCORS: true,//支持图片跨域
+  }).then((canvas) => {
+    let contentWidth = canvas.width;
+    let contentHeight = canvas.height;
+    // 一页pdf显示html页面生成的canvas高度;
+    let pageHeight = contentWidth / 592.28 * 841.89;
+    // 未生成pdf的html页面高度
+    let leftHeight = contentHeight;
+    // pdf页面偏移
+    let position = -8;
+    // html页面生成的canvas在pdf中图片的宽高（a4纸的尺寸[595.28,841.89]）
+    let imgWidth = 595.28;
+    let imgHeight = 592.28 / contentWidth * contentHeight;
 
-// 打印
+    let pageData = canvas.toDataURL('image/jpeg', 1);
+    let pdf = new jsPDF('', 'pt', 'a4');
+
+    // 有两个高度需要区分，一个是html页面的实际高度leftHeight，和生成pdf的页面高度(841.89)pageHeight
+    // 当内容未超过pdf一页显示的范围，无需分页
+    if (leftHeight < pageHeight) {
+      pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight);
+    } else {
+      while (leftHeight > 0) {
+        pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
+        leftHeight -= (pageHeight + 20);
+        //原来高度是841.89，因为页面之间有20的padding,还有多余的7是因为页面比A4的高多了一点
+        position -= 869.89;
+        // 避免添加空白页
+        if (leftHeight > 841.89) {
+          pdf.addPage();
+        }
+      }
+    }
+    pdf.save('运行报告.pdf');
+  });
+}
+
+
+
+// 打印 start
 
 const printRef = useRef()
-
-  //打印报告
-  const reportPrint = () => {
-    let printDom = document.getElementById("printRef");
-    console.log(printRef.current.scrollHeight)
-    html2canvas(printDom, {
-      height: printDom.scrollHeight,
-      windowHeight: printDom.scrollHeight,
-      allowTaint: true,
-      
-      scale: "1", //设置放大倍数
-      // dpi: "192",
-      background: "#fff",
-      // 开启跨域配置
-      useCORS: true, //支持图片跨域
-    }).then((canvas) => {
-      let pageData = canvas.toDataURL("image/jpeg", 1.0);
-      printJS({
-        printable: pageData,
-        type: "image",
-        style: `@media print { @page {size: auto; margin: 0; } body{margin:0 5px}}`// 去除页眉页脚
-      });
-    });
-  };
-
-
-
 const reactToPrintContent = useCallback(() => {
   return printRef.current;
 }, [printRef.current])
 
 const handlePrint = useReactToPrint({
   content: reactToPrintContent,
-  
- // copyStyles: false,
- // pageStyle: () => '.title {color: "#ff7313"}',
- // removeAfterPrint: false,
- // copyStyles: false,
- // bodyClass: "right, header, main",
- // copyStyles: false,
- // onAfterPrint: () => printRef.current = null,
-  
 })
-/* const getPageMargins = () => {
-  return `@page { margin: 20px !important; }`;
-}; */
-const onPrint = () => {
-  console.log(loading)
+ 
+const onPrint = () => {  
   try {
-    if(!loading) return message.warning('请先生成报告', 0.3)
-    //reportPrint()
+    if(!loading) return message.warning('请先生成报告', 0.3)  
     handlePrint()
   } catch (error) {
     console.log(error)
   }
 
 }
+
+// 打印 end
   const getReport = async () => {
      try {
       
@@ -556,14 +569,14 @@ const onPrint = () => {
              <div className='btns'>
                     <CustButton wh="192px" src="createrpt" onClick={getReport}>生成报告</CustButton>
                     <CustButton wh="192px" src="print" onClick={onPrint}>打印报告</CustButton>
-                    <CustButton wh="192px" src="export">导出报告</CustButton>
+                    <CustButton wh="192px" src="export" onClick={downloadReport}>导出报告</CustButton>
              </div>
                
               
           </Titlelayout>      
           </div>
           <div>
-          <div className='right' ref={printRef}>
+          <div className='right' ref={printRef} id="printRef">
                          
                <Front>
                    <div className='title'>
