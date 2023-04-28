5fd98ff953c5719c0aa5889e84b88d0a4dbba8d1
53328085 <53328085@qq.com>
2023-04-28 08:51:34 +0800

    bug修复

--- src/pages/test/fform.js
+++ src/pages/test/fform.js
@@ -1,34 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
-const Printbox = styled.div`
-  height: 400px;
- 
-  h1 {
-    color: #69c0ff;
-  }
-  .pagebreak {
-    page-break-after: always;
-  }
-`
+
 export default function Index() {
   return (
-    <div>
-      
-          <nav>
-            <h1>你好</h1>
-            <h2>哈喽</h2>
-          </nav>
-          <h1>第一页</h1>
-          <p >
-            <a href='http://www.sina.com.cn'>新浪</a>
-          </p>
-          <hr className='pagebreak' /> 
-          <h1>第二页</h1>
-         
-          <hr className='pagebreak' /> 
-          <h1>第三页</h1>
-         <button onClick={() => window.print()}>print</button>
-       
-    </div>
+    <div>fform</div>
   )
 }
