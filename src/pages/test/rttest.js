d10f774acc9f65100147bdabf6b790b54b854c6f
53328085 <53328085@qq.com>
2023-05-08 16:44:22 +0800

    react-router-dom 升级，其他文字修改

--- src/pages/test/rttest.js
+++ src/pages/test/rttest.js
@@ -1,15 +1,17 @@
  
 import React from 'react'
-import {Link, useLocation, NavLink, useOutletContext, useParams} from 'react-router-dom'
+import {Link, useLocation, NavLink, useOutletContext, useParams, useLoaderData} from 'react-router-dom'
 export default function Index() {
-  const [count, setCount] = useOutletContext()
-  const increment = () => setCount((c) => c + 1);
+ // const [count, setCount] = useOutletContext()
+  //const increment = () => setCount((c) => c + 1);
   const useId = useParams()
+ // const data = useLoaderData()
+  //console.log(data)
   console.log(useId)
   return (
     <div>
        <h>正泰物联</h>
-       <button onClick={increment}>{JSON.stringify(count, 3)}</button>;
+     
     </div>
     
   )
