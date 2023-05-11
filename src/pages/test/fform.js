f98538b6cedf7c740073784e0a2a1838833ae891
53328085 <53328085@qq.com>
2023-05-11 16:59:37 +0800

    不重要的更新

--- src/pages/test/fform.js
+++ src/pages/test/fform.js
@@ -1,23 +1,52 @@
-import React from 'react'
-import Cont from './cont'
+import React, {useEffect, useState} from 'react'
+import styled from 'styled-components';
+import log from './log.png'
+const Ccanvas = styled.canvas`
+ && {
+  border: 1px solid #d7d7d7;
 
-import {useSelector, useDispatch} from 'react-redux'
-import {setzl , zltest} from '@redux/reduxTest.js'
-
+ }
+`
 export default function Index() {
-  console.log('render')
-  const name = useSelector(zltest)
-  const dispatch = useDispatch()
-  const onChange = () => {
-     dispatch(setzl('zzl'))
-  }
+  const [info, setInfo] = useState('')
+   
+  useEffect(() => {
+    var canvas = document.querySelector("#canvas")
+    if(canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+      let img = new Image()
+      img.src=log
+     /*  img.onload = () => {
+        console.log(2222)
+        try {
+          var ptrn = ctx.createPattern(img, 'repeat');
+          ctx.fillStyle = ptrn;
+          ctx.fillRect(10, 10, 160, 160);
+        } catch (error) {
+          console.log(error)
+        }
+       
+      } */
+      ctx.shadowOffsetX = 4;
+      ctx.shadowOffsetY = 4;
+      ctx.shadowBlur = 2;
+      ctx.shadowColor = "rgba(0, 0, 0, 0.5)";
+    
+      ctx.beginPath();
+      ctx.arc(50, 50, 30, 0, Math.PI*2, true);
+      ctx.arc(50, 50, 15, 0, Math.PI*2, true);
+      ctx.fill("evenodd");
+      ctx.font="50px serif";
+      ctx.textAlign = "left"
+      ctx.strokeText("正泰物联", 100, 65)
+      
+    }else {
+       setInfo('浏览器不支持canvas,请安装新版浏览器')
+    }
+  })
   return (
     <div>
-       <h1>{name}</h1>
-       <p>
-        <button onClick={onChange}>CHANGE</button>
-       </p>
-       <Cont id="ddd" /> 
+       <canvas id="canvas" width="550" height="550" style={{border: '1px solid #d7d7d7'}}>{info}</canvas>
     </div>
   )
 }
