4373c2874ae06368d987fd30d6037f325e268a8c
ybgsh <ybgsh@chint.com>
2023-05-22 10:32:50 +0800

    样式调整

--- src/pages/storage/environment/index.jsx
+++ src/pages/storage/environment/index.jsx
@@ -1,27 +1,30 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import styled from 'styled-components'
 import style from './style.module.less'
 import { Button, DatePicker, message, Form, Select } from 'antd'
 import moment from 'moment'
-import { CaretLeftOutlined, CaretRightOutlined, SearchOutlined  } from '@ant-design/icons'
+import { CaretLeftOutlined, CaretRightOutlined, SearchOutlined } from '@ant-design/icons'
 import CustModal from '@com/useModal'
 import * as echarts from 'echarts'
 import bgImg from './imgs/background.png'
+import icon from './imgs/icon.png'
+import iconHover from './imgs/icon_hover.png'
 import { StorageEnvironmentRuntime, SiteManagerDesigner } from '@api/api'
 import { useSelector, useDispatch } from 'react-redux'
 import { selectProjectId, selectOneLevel, levelDefaultLabel, selectOneLevelDefaultId, setCurrentlevel } from '@redux/systemconfig.js'
+import { useReactive } from 'ahooks'
 
 export default function Index() {
 
   const TempRef = useRef()
   const lineRef = useRef()
-  const { queryEnvironmentInfo, queryTrends } =  StorageEnvironmentRuntime
+  const { queryEnvironmentInfo, queryTrends } = StorageEnvironmentRuntime
   let time = new Date()
   let year = time.getFullYear()
   let month = time.getMonth() + 1
   month = month < 10 ? '0' + month : month
   let day = time.getDate()
-  day = day < 10 ? '0' + day : day 
+  day = day < 10 ? '0' + day : day
 
   const today = year + '-' + month + '-' + day
   const [form] = Form.useForm()
@@ -38,9 +41,9 @@ export default function Index() {
 
   //页面组件
   const CustomCss = styled.div`
-    width: 234px;
+    width: 260px;
     height: 680px;
-    margin-right: 148px;
+    margin-right: 122px;
     background-image: url('${bgImg}');
     background-size: 100% 100%;
     position:relative;
@@ -58,7 +61,7 @@ export default function Index() {
       position: absolute;
       left: 0;
       bottom: 456px;
-      width: 234px;
+      width: 260px;
       height: 24px;
       text-align: center;
       font-size: 14px;
@@ -69,12 +72,13 @@ export default function Index() {
       position: absolute;
       left: 0;
       bottom: 0;
-      width: 234px;
+      width: 260px;
       height: 456px;
-      padding: 16px;
+      padding: 10px;
+      padding-top: 16px;
       .item{
         margin-bottom: 16px;
-        width: 200px;
+        width: 240px;
         background-color: #fff;
         .monitorTitle{
           width: 100%;
@@ -84,12 +88,38 @@ export default function Index() {
           font-size:12px;
           color: #fff;
         }
+        .leftIcon{
+            width: 16px;
+            height: 8px;
+            transform: rotate(-90deg);
+            background-image: url('${icon}');
+            background-size: 100% 100%;
+            background-repeat: no-repeat;
+            &:hover{
+              background-image: url('${iconHover}');
+            }
+            z-index: 1000;
+          }
+          .rightIcon{
+            width: 16px;
+            height: 8px;
+            transform: rotate(90deg);
+            background-image: url('${icon}');
+            background-size: 100% 100%;
+            background-repeat: no-repeat;
+            &:hover{
+              background-image: url('${iconHover}');
+            }
+            z-index: 1000;
+          }
         .temData{
           padding-top: 6px;
           display: flex;
           align-items: center;
+          justify-content: space-around;
+          
           .tem{
-            width: 50%;
+            width: 90px;
             font-size: 12px;
             color: #515151;
             padding-left: 24px;
@@ -109,7 +139,7 @@ export default function Index() {
           color: #000;
           display: flex;
           align-items: center;
-          justify-content: space-between;
+          justify-content: space-around;
           span{
             display: inline-block;
             min-width: 20px;
@@ -143,72 +173,154 @@ export default function Index() {
   `
   const [count, setCount] = useState(0)
   const transLeft = () => {
-    if((count)<= 0) return;
+    if ((count) <= 0) return;
     setCount(count - 1)
   }
   const transRight = () => {
-    if((count + 4)>= length) return;
+    if ((count + 4) >= length) return;
     setCount(count + 1)
   }
   const CustomData = props => {
-    let {data} = props
+    let { data } = props
+    let { air, env, fireMonitor, waterMonitor } = data
+    const state = useReactive({
+      airCount: 0,
+      envCount: 0,
+      fireCount: 0,
+      waterCount: 0,
+    })
+
+    const changePrefer = (name) => {
+      if (name == 'air') {
+        if (state.airCount == 0) {
+          return;
+        } else {
+          state.airCount--;
+        }
+      }
+      if (name == 'env') {
+        if (state.envCount == 0) {
+          return;
+        } else {
+          state.envCount--;
+        }
+      }
+      if (name == 'fire') {
+        if (state.fireCount == 0) {
+          return;
+        } else {
+          state.fireCount--;
+        }
+      }
+      if (name == 'water') {
+        if (state.waterCount == 0) {
+          return;
+        } else {
+          state.waterCount--;
+        }
+      }
+    }
+
+    const changeNext = (name) => {
+      if (name == 'air') {
+        if (state.airCount == air.length) {
+          return;
+        } else {
+          state.airCount++;
+        }
+      }
+      if (name == 'env') {
+        if (state.envCount == env.length) {
+          return;
+        } else {
+          state.envCount++;
+        }
+      }
+      if (name == 'fire') {
+        if (state.fireCount == fireMonitor.length) {
+          return;
+        } else {
+          state.fireCount++;
+        }
+      }
+      if (name == 'water') {
+        if (state.waterCount == waterMonitor.length) {
+          return;
+        } else {
+          state.waterCount++;
+        }
+      }
+    }
+
     return <CustomCss>
       <div className='name'>{data.name}</div>
       <div className='itemTitle'>环境监控</div>
       <div className='itemData'>
-        <div className='item' style={{cursor:'pointer'}} onClick={()=>showChart(data.id)}>
-          <div className='monitorTitle' style={{backgroundColor:'#237ae4', border: '1px solid #237ae4'}}>空调监控</div>
+        <div className='item' style={{ cursor: 'pointer' }}>
+          <div className='monitorTitle' style={{ backgroundColor: '#237ae4', border: '1px solid #237ae4' }}>{air[state.airCount]?.name || '空调监控'}</div>
           <div className='temData'>
+            {(air.length <= 1 || state.airCount == 0) ? <div style={{ width: 8, height: '16px' }}></div> : <div className='leftIcon' onClick={() => changePrefer('air')}></div>}
+            <div className='tem' onClick={() => showChart(data.id)}>
+              <span>温度</span>
+              <div>
+                <span style={{ fontSize: 20 }}>{air[state.airCount]?.temp || '/'}</span>
+              </div>
+            </div>
+            <div className='tem' onClick={() => showChart(data.id)}>
+              <span>湿度</span>
+              <div>
+                <span style={{ fontSize: 20 }}>{air[state.airCount]?.humidity || '/'}</span>
+              </div>
+            </div>
+            {(air.length <= 1 || state.airCount >= air.length - 1) ? <div style={{ width: 8, height: '16px' }}></div> : <div className='rightIcon' onClick={() => changeNext('air')}></div>}
+          </div>
+          <div className='tempTime'> {air[state.airCount]?.reportTime || '/'}</div>
+        </div>
+        <div className='item' style={{ cursor: 'pointer' }}>
+          <div className='monitorTitle' style={{ backgroundColor: '#237ae4', border: '1px solid #237ae4' }}>{env[state.envCount]?.name || '环境温湿度'}</div>
+          <div className='temData'>
+            {(env.length <= 1 || state.envCount == 0) ? <div style={{ width: 8, height: '16px' }}></div> : <div className='leftIcon' onClick={() => changePrefer('env')}></div>}
             <div className='tem'>
               <span>温度</span>
               <div>
-                <span style={{fontSize:20}}>{ data.airTemp }</span>
+                <span style={{ fontSize: 20 }}>{env[state.envCount]?.temp || '/'}</span>
               </div>
             </div>
             <div className='tem'>
               <span>湿度</span>
               <div>
-                <span style={{fontSize:20}}>{ data.airHumidity }</span>
+                <span style={{ fontSize: 20 }}>{env[state.envCount]?.humidity || '/'}</span>
               </div>
             </div>
+            {(env.length <= 1 || state.envCount >= env.length - 1) ? <div style={{ width: 8, height: '16px' }}></div> : <div className='rightIcon' onClick={() => changeNext('env')}></div>}
           </div>
-          <div className='tempTime'> {data.airInformTime}</div>
-        </div>
-        <div className='item' style={{cursor:'pointer'}}>
-          <div className='monitorTitle' style={{backgroundColor:'#237ae4', border: '1px solid #237ae4'}}>环境温湿度</div>
-          <div className='temData'>
-            <div className='tem'>
-              <span>温度</span>
-              <div>
-                <span style={{fontSize:20}}>{ data.environmentTemp }</span>
-              </div>
-            </div>
-            <div className='tem'>
-              <span>湿度</span>
-              <div>
-                <span style={{fontSize:20}}>{ data.environmentHumidity }</span>
-              </div>
-            </div>
-          </div>
-          <div className='tempTime'> {data.environmentInformTime}</div>
+          <div className='tempTime'> {env[state.envCount]?.reportTime || '/'}</div>
         </div>
         <div className='item'>
-          <div className='monitorTitle' style={{backgroundColor:'#093', border: '1px solid #093', height: 32, lineHeight:'32px'}}>水浸监控</div>
+          <div className='monitorTitle' style={{ backgroundColor: '#093', border: '1px solid #093', height: 32, lineHeight: '32px' }}>{waterMonitor[state.waterCount]?.name || '水浸监控'}</div>
           <div className='monitorData'>
-            <span>{ data.waterOutInformTime }</span>
-            <span>{ data.waterOutWarning  }</span>
+            {(waterMonitor.length <= 1 || state.waterCount == 0) ? <div style={{ width: 8, height: '16px' }}></div> : <div className='leftIcon' onClick={() => changePrefer('water')}></div>}
+            <span>{waterMonitor[state.waterCount]?.warningTime || '/'}</span>
+            <span>{waterMonitor[state.waterCount]?.warning || '/'}</span>
+            {(waterMonitor.length <= 1 || state.waterCount >= waterMonitor.length - 1) ? <div style={{ width: 8, height: '16px' }}></div> : <div className='rightIcon' onClick={() => changeNext('water')}></div>}
           </div>
+          
         </div>
         <div className='item'>
-          <div className='monitorTitle' style={{backgroundColor:'#093', border: '1px solid #093', height: 32, lineHeight:'32px'}}>灭火器监控</div>
+          <div className='monitorTitle' style={{ backgroundColor: '#093', border: '1px solid #093', height: 32, lineHeight: '32px' }}>{fireMonitor[state.fireCount]?.name || '灭火器监控'}</div>
           <div className='monitorData'>
-            <span>{ data.fireInformTime }</span>
-            <span>{ data.fireWarning }</span>
+            {(fireMonitor.length <= 1 || state.fireCount == 0) ? <div style={{ width: 8, height: '16px' }}></div> : <div className='leftIcon' onClick={() => changePrefer('fire')}></div>}
+            <span>{fireMonitor[state.fireCount]?.warningTime || '/'}</span>
+            <span>{fireMonitor[state.fireCount]?.warning || '/'}</span>
+            {(fireMonitor.length <= 1 || state.fireCount >= fireMonitor.length - 1) ? <div style={{ width: 8, height: '16px' }}></div> : <div className='rightIcon' onClick={() => changeNext('fire')}></div>}
           </div>
         </div>
       </div>
     </CustomCss>
   }
+
+
+
 
   //页面数据
   const [storageData, setStorageData] = useState([])
@@ -237,7 +349,7 @@ export default function Index() {
   }
   const changeSite = val => {
     siteList.map(item => {
-      if(item.id == val){
+      if (item.id == val) {
         setSiteName(item.name)
       }
     })
@@ -254,7 +366,7 @@ export default function Index() {
   }, [])
   const changeArea = (val) => {
     areaList.map(item => {
-      if(item.id == val){
+      if (item.id == val) {
         dispatch(setCurrentlevel(item))
       }
     })
@@ -264,65 +376,65 @@ export default function Index() {
 
   const getFromHeader = () => {
     queryEnvironmentInfo(projectId, headForm.getFieldValue('areaId'), headForm.getFieldValue('siteId')).then(res => {
-      if(res.success){
-        if(res.data){
+      if (res.success) {
+        if (res.data) {
           setStorageData(res.data)
-        }else{
+        } else {
           setStorageData([])
         }
-      }else{
+      } else {
         message.error(res.errMsg)
       }
     })
   }
 
-   //弹窗
+  //弹窗
   const [roomId, setRoomId] = useState(0)
   const getTrends = (projectId, roomId, date) => {
-    queryTrends(projectId, roomId, date).then(res=> {
-      let {success, data} = res
-      if(success){
-        if(data){
+    queryTrends(projectId, roomId, date).then(res => {
+      let { success, data } = res
+      if (success) {
+        if (data) {
           let tempTrends = []
           let humidityTrends = []
           let time = []
           time = data.x
           tempTrends = data.y
           humidityTrends = data.y1
-          setTimeout(()=>{
+          setTimeout(() => {
             drawLine({
               time,
               humidityTrends,
               tempTrends,
             })
           })
-        }else{
+        } else {
           drawLine({
-            time:[],
-            tempTrends:[],
-            humidityTrends:[],
+            time: [],
+            tempTrends: [],
+            humidityTrends: [],
           })
         }
-      }else{
+      } else {
         message.error(res.errMsg)
       }
     })
   }
   const showChart = (id) => {
     setRoomId(id)
-    form.setFieldValue('date', moment(today,'YYYY-MM-DD'))
-    getTrends(projectId, id, today) 
+    form.setFieldValue('date', moment(today, 'YYYY-MM-DD'))
+    getTrends(projectId, id, today)
     TempRef.current.onOpen()
   }
- 
+
   const onOk = () => {
     TempRef.current.onCancel()
   }
-  const drawLine = (lineData)=> {
+  const drawLine = (lineData) => {
     let lineChart = echarts.init(lineRef.current);
     lineChart.clear()
     lineChart.setOption({
-      color:['#237ae4', '#093'],
+      color: ['#237ae4', '#093'],
       tooltip: {
         trigger: 'axis',
         axisPointer: {
@@ -343,8 +455,8 @@ export default function Index() {
       xAxis: {
         type: 'category',
         boundaryGap: true,
-        axisTick:{
-          alignWithLabel:true
+        axisTick: {
+          alignWithLabel: true
         },
         data: lineData.time
       },
@@ -353,8 +465,8 @@ export default function Index() {
         // min: 24
         scale: true, //自适应
       },
-      dataZoom:{
-        type:'slider',
+      dataZoom: {
+        type: 'slider',
         height: 24
       },
       series: [
@@ -362,20 +474,20 @@ export default function Index() {
           name: '温度(℃)',
           data: lineData.tempTrends,
           type: 'line',
-          symbol:'circle', 
+          symbol: 'circle',
         },
         {
           name: '湿度(%)',
           data: lineData.humidityTrends,
           type: 'line',
-          symbol:'circle', 
+          symbol: 'circle',
         }
       ]
     }, true)
   }
   const onSearch = () => {
     const date = form.getFieldValue('date').format('YYYY-MM-DD')
-    getTrends(projectId, roomId, date) 
+    getTrends(projectId, roomId, date)
   }
 
   //defaultValue={moment(today,'YYYY-MM-DD')}
@@ -415,24 +527,24 @@ export default function Index() {
         <div className={style.yaxis}></div>
         <div className={style.xaxis}></div>
         <div className={style.dataList}>
-          <div className={style.transLate} style={{ width: (parseInt(storageData.length / 4) + 1) * 100 + '%', left: (-(count * 382) + 65)}}>
-            { storageData.map((item, index) =>{
+          <div className={style.transLate} style={{ width: (parseInt(storageData.length / 4) + 1) * 100 + '%', left: (-(count * 382) + 65) }}>
+            {storageData.map((item, index) => {
               return <CustomData data={item} key={index}></CustomData>
-            } ) }
+            })}
           </div>
-          <LeftButton onClick={()=>transLeft()}></LeftButton>
-          <RightButton onClick={()=>transRight()}></RightButton>
+          <LeftButton onClick={() => transLeft()}></LeftButton>
+          <RightButton onClick={() => transRight()}></RightButton>
         </div>
       </div>
-      <CustModal title='温湿度趋势' ref={TempRef}  mold="cust" width={1680}  onOk={()=>onOk()}>
-        <div style={{ position:'absolute', right: 32, top:32, display: 'flex',alignItems:'center'}}>
+      <CustModal title='温湿度趋势' ref={TempRef} mold="cust" width={1680} onOk={() => onOk()}>
+        <div style={{ position: 'absolute', right: 32, top: 32, display: 'flex', alignItems: 'center' }}>
           <span>日期</span>
           <Form name='addForm' form={form}>
             <Item name='date' label=''>
-              <DatePicker style={{width: 182, margin: '0 16px' }} ></DatePicker>
+              <DatePicker style={{ width: 182, margin: '0 16px' }} ></DatePicker>
             </Item>
           </Form>
-          <Button type='primary' icon={<SearchOutlined />} style={{width: 96}} onClick={()=> onSearch()}>查询</Button>
+          <Button type='primary' icon={<SearchOutlined />} style={{ width: 96 }} onClick={() => onSearch()}>查询</Button>
         </div>
         <div className={style.lineChart} ref={lineRef}></div>
       </CustModal>
