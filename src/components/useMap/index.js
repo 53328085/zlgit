e9dc89083630f778adfcaa6f47c425fbb0c3a981
53328085 <53328085@qq.com>
2023-05-30 10:58:34 +0800

    修改天地图搜索后不能拖动的问题

--- src/components/useMap/index.js
+++ src/components/useMap/index.js
@@ -6,7 +6,7 @@ import React, {useState, useEffect, useRef, forwardRef, useImperativeHandle} fro
 import {useSelector} from 'react-redux'
 import {systemConfigInfo} from '@redux/systemconfig'
 import {message} from 'antd'
-import {Tadi} from '@api/api'
+ 
   function Index(props, ref) {
   const {lngLat, value,setAaddress, onChange} = props
  
@@ -16,7 +16,7 @@ import {Tadi} from '@api/api'
  
   let {companyName} = useSelector(systemConfigInfo)
   const [zoom] = useState(18)
-  const [mapref, setMapref] = useState()
+
 
   const geocoder = new T.Geocoder();
   const MapOptions = {
@@ -24,34 +24,50 @@ import {Tadi} from '@api/api'
     minZoom: 2,
     maxZoom: 18,
   }
-  const map = mapref ? new T.Map(mapref, MapOptions) : null
+ 
+  let mapref = useRef()
+  let map = mapref.current? new T.Map(mapref.current, MapOptions) : null
+
+  const addmarker = (latlng, text='') => {
+     map.clearOverLays()
+     let marker = new  T.Marker(latlng);
+     let label = new T.Label({
+      text,
+      position: latlng,
+      offset: new T.Point(-9, 0),
+     })
+     map.addOverLay(marker);
+     map.addOverLay(label)
+
+  }
 
 
-  const searchResult = (result) =>{
-		if(result.getStatus() == 0){
-      map.clearOverLays();
-			map.panTo(result.getLocationPoint(), 16);
-			//创建标注对象
-	        var marker = new T.Marker(result.getLocationPoint());
-	        //向地图上添加标注
-
-	        map.addOverLay(marker);
+  const searchResult = (result) =>{   
+		if(result.getStatus() == 0){    
+			map.panTo(result.getLocationPoint(), 16);    
+      addmarker(result.getLocationPoint(), result.location?.keyWord)
 		}else{
 			message.error({count: result.getMsg()});
 		}
 		
 	}
-  const serachMap = (value) => {
-   
-		geocoder.getPoint(value, searchResult)
+  const serachMap = (value) => {   
+    try {
+      map.clearOverLays();
+      geocoder.getPoint(value, searchResult)
+    } catch (error) {
+      console.log(error)
+    }
+		
   }
   const mapClick = (result) =>  {  
+     
     if(result.getStatus() == 0) {
     
       let {addressComponent,  location: {lon, lat}} = result
       let {city, province, county, address, address_distance} = addressComponent
       setAaddress && setAaddress({lng: lon, lat, address: result.getAddress(), province, city, district: county, street: address, streetNumber:address_distance})
-
+      addmarker(new T.LngLat(lon, lat), result.getAddress())
     }else {
 
       setAaddress && setAaddress({})
@@ -64,32 +80,26 @@ import {Tadi} from '@api/api'
     serachMap
   }))
   useEffect(() => {
-     if(!mapref) return
+     if(!mapref.current || !map) return
     
      let latlng = lng&&lat ?new T.LngLat(lng, lat) :  new T.LngLat(120.22830511467954, 30.21229461177818)
      map.centerAndZoom(latlng, zoom); // 初始化
-     let label = new T.Label({
-        text: lng&&lat ? '' : companyName,
-        position: latlng,
-        offset: new T.Point(-9, 0)
-
-     })
-     let marker = new T.Marker(latlng)
-     map.addOverLay(label)
-     map.addOverLay(marker)
-     marker.disableDragging(); 
-     map.enableDrag();
+    
+     
      let geocode = new T.Geocoder();
-     map.addEventListener("click", (e) => {     
+
+
+     geocode.getLocation(latlng,mapClick)
+     map.addEventListener("click", (e) => {   
+     
+     
       geocode.getLocation(e.lnglat,mapClick)
      });
-
-     return () => {
-      map.removeEventListener("click",mapClick)
-     }
-  }, [mapref])
+   
+  
+  }, [mapref.current, map])
   return (
-    <div style={{flex: 1, height: '100%'}} ref={(node) => setMapref(node)} id="mapBox" >
+    <div style={{flex: 1}} ref={mapref} id="mapBox" >
 
     </div>
   )
