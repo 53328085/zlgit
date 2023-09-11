f4526bf5bc90dc55d60ad08c2bd833c8e2152e83
53328085 <53328085@qq.com>
2023-09-11 15:39:42 +0800

    添加运维管理 排班路由

--- src/redux/systemconfig.js
+++ src/redux/systemconfig.js
@@ -165,8 +165,8 @@ export const comSetFirst  = state => state.system.menus?.comSet[0]
 //export const allsinderRunMenus  = state => state.system.menus?.allsinderRunMenus
 export const selectProjectId = state => state.system.menus?.projectId
 export const selectOneLevel = state => state.system.onelevel
-export const selectOneLevelDefaultId = state => state.system.currlevel?.id || state.system.onelevel[0]?.id
-export const levelDefaultLabel = state => state.system.currlevel?.levelName || state.system.onelevel[0]?.levelName
+export const selectOneLevelDefaultId = state => state.system.onelevel[0]?.id || state.system.currlevel?.id
+export const levelDefaultLabel = state =>  state.system.onelevel[0]?.levelName || state.system.currlevel?.levelName
 export const selectshifts = state => state.system.shifts
 export const publishState = state => {
   return  state.system.publishState == 1
