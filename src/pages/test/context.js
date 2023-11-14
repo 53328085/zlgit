ff8dc0b3382b56b6f547d5ef54f8781fc70463cd
53328085 <53328085@qq.com>
2023-11-14 16:13:43 +0800

    删除一些测试文件

--- src/pages/test/context.js
+++
@@ -1,8 +0,0 @@
-import {createContext} from 'react'
-
-export const ageContext = createContext(30);
-
-export const jobContext = createContext('web工程师')
-export const levelContext = createContext(0)
-
-export const objCOntext = createContext({})
