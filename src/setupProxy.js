144c61e23ec96a7b43af7ad5ce17158999d517aa
ybmq <ybmq@2021>
2023-12-18 08:50:49 +0800

    commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use( 
         createProxyMiddleware("/api/V1", {
             // target: 'http://10.5.7.60:4155',   // https://dmm.chint.com/ (正式)   // http://10.5.7.60:4155（测试）
-           target: 'https://dmm.chint.com/', // 王建  
+           target:'http://10.5.7.60:4155', // 王建  
             changeOrigin : true,
             ws: true,
             pathRewrite: {
