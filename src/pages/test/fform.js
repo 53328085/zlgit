b7d3d6846404fee87553462d143436731d47ded8
53328085 <53328085@qq.com>
2024-03-27 08:18:52 +0800

    国际化

--- src/pages/test/fform.js
+++ src/pages/test/fform.js
@@ -1,7 +1,11 @@
-import React, {useRef, useEffect} from 'react'
+import React, {useRef, useEffect, useState} from 'react'
+import {Typography} from 'antd'
 import moment from 'moment';
 import {useTranslation, Trans, Translation} from 'react-i18next';
+import i18t from '../../i18n'
 import styled, {keyframes} from 'styled-components';
+console.log(i18t.resolvedLanguage);
+const {Paragraph} = Typography
 const rota = keyframes`
   from {
     transform: translate3d(0 0 0);
@@ -30,17 +34,24 @@ const Animation=styled.div`
   }
 `
 export default function Index() {
+  const [ltr, setLtr] = useState('ltr')
   const {t, i18n} = useTranslation();
+  const de = i18n.getFixedT('de', 'zd');
+  const transform = () => {
+     i18t.dir("ar")
+     setLtr(i18t.dir())
+  }
   const lngs = {
     en: { nativeName: 'English' },
     zh: { nativeName: '中文' }
   };
   const ref = useRef()
   useEffect(() => {
-    console.log(ref.current.offsetHeight);
+   // console.log(ref.current.offsetHeight);
   })
   return (
-    <div>
+    <div dir={ltr}>
+      fwefsdfsdfsasasasasasasasasasasasasasasasasasasasasasa
       <header>
       <select onChange={(evt) => {
           i18n.changeLanguage(evt.target.value)
@@ -52,21 +63,22 @@ export default function Index() {
         </select>
       </header>
       <main>
-
-        <p>{t('welcome')}</p>
-         <h1>{t('author')}</h1>
+         <button onClick={transform}>英文</button>
+        <p>{t("welcome")}</p>
+         <h1>{t('test:author')}</h1>
          <h1>
           {t('currentTime', {time: moment()})}
          </h1>
+             <Paragraph>{t('key1_interval', { postProcess: 'interval', count: 100 })}</Paragraph>
          <div style={{height: "105px", overflow:"hidden"}}>
-         <Animation ref={ref}>
+        {/*  <Animation ref={ref}>
          
             {Array.from({length: 7}, (c,i) => <p>{i}</p>)}
              <div style={{height: '105px', overflow: "hidden"}}>
               {Array.from({length: 7}, (c,i) => <p>{i}</p>)}
              </div>
           
-         </Animation>
+         </Animation> */}
          </div>
       </main>
     </div>
