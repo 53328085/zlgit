52771df18e6d54293e4ae18376d41e84bbc71920
53328085 <53328085@qq.com>
2024-05-20 19:18:43 +0800

    碳排管理--预警配置

--- src/pages/carbon/configure/warningSetting/index.jsx
+++ src/pages/carbon/configure/warningSetting/index.jsx
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import Pagecount from '@com/pagecontent'
 import styled from 'styled-components'
-import {Form,  Space, DatePicker, Tooltip} from 'antd'
+import {Form,  Space, DatePicker, Tooltip, InputNumber, Input} from 'antd'
 import Usetable from "@com/useTable"
 import Titlelayout from "@com/titlelayout"
  import {  useDeleteStrategyMutation,
@@ -13,6 +13,9 @@ import {CustButtonT,CustLink} from "@com/useButton"
 import {Cdivider} from "@com/comstyled"
 import {useSelector} from 'react-redux'
 import {selectProjectId, enterprise} from '@redux/systemconfig'
+import CModal from "@com/useModal"
+
+ 
 const Mainbox = styled.div`
   margin-top: 16px;
     padding-top: 16px;
@@ -22,70 +25,98 @@ const Mainbox = styled.div`
     gap: 16px;
     flex:1;
 `
-
-export default function Index() { 
-
-  const columns = [
+const columns = [
+  {
+      title: '序号',
+      dataIndex: 'year',
+       key: ''
+  },
+ 
     {
-        title: '序号',
-        dataIndex: 'year',
-         key: ''
-    },
-   
-      {
-        title: '规则名称',
-        dataIndex: 'ruleName',
-         key: 'ruleName'
-     },
-     {
-      title: '预期周期',
-      dataIndex: 'expectedPeriod',
-       key: 'expectedPeriod'
+      title: '规则名称',
+      dataIndex: 'ruleName',
+       key: 'ruleName'
    },
    {
-    title: '预期限制计算方法',
-    dataIndex: 'Calculation',
-     key: 'Calculation'
- }, 
+    title: '预期周期',
+    dataIndex: 'expectedPeriod',
+     key: 'expectedPeriod'
+ },
  {
-  title: '限值及等级',
-  dataIndex: 'LimitsAndLevels',
-   key: 'LimitsAndLevels'
+  title: '预期限制计算方法',
+  dataIndex: 'Calculation',
+   key: 'Calculation'
 }, 
 {
-  title: '是否启用',
-  dataIndex: 'Enabled',
-   key: 'Enabled'
+title: '限值及等级',
+dataIndex: 'LimitsAndLevels',
+ key: 'LimitsAndLevels'
 }, 
-   {
-    title: "操作",
-    dataIndex: 'total',
-      key: 'total',
-      align: 'center',
-      render: (text,record) => {
-        return <Space><CustLink text="edit" /></Space>
-      }
-   }
-  ]
-  console.log('render')
+{
+title: '是否启用',
+dataIndex: 'Enabled',
+ key: 'Enabled'
+}, 
+ {
+  title: "操作",
+  dataIndex: 'total',
+    key: 'total',
+    align: 'center',
+    render: (text,record) => {
+      return <Space><CustLink text="edit" /></Space>
+    }
+ }
+]
+export default function Index() { 
+
+
+  
   const [form] = Form.useForm()
   const {id} = useSelector(enterprise)
- 
-  const {isSuccess, data} = useStrategyAllQuery()
+
+  const [title, setTitle] = useState('')
+  const [tableData, setTableData] = useState([])
+  const ref= useRef()
+/*   const {isSuccess, data} = useStrategyAllQuery()
   let tableData = []
   if(isSuccess && Array.isArray(data?.data)) {
      console.log(data?.data)
   }else {
     tableData = []
   }  
- 
+  */
+ const onAdd =() => {
+  setTitle('新增预警策略配置')
+   ref.current.onOpen()
+ }
   const CTitle = (
     <div style={{display: 'flex', alignItems: "center", justifyContent: "space-between"}}>
         <span>预警策略配置</span>
-        <Space><CustButtonT text="new"   /></Space>
+        <Space><CustButtonT text="new" onClick={onAdd}   /></Space>
     </div>
   )
  
+  const onOk = async () => {
+      try {
+        let values =  await form.validateFields()
+        console.log(values)
+      } catch (error) {
+        
+      }
+  }
+  const rules = [
+    {
+      required: true
+    }
+  ]
+  const onChange = (v) => {
+    let max = (v-0.01).toFixed(2)
+    form.setFieldValue(['2', 'LimitValueMax'],max)
+  }
+  const onChangel = (v) => {
+    let max = (v+0.01).toFixed(2)
+    form.setFieldValue(['2', 'LimitValueMin'],max)
+  }
   return (
     <Pagecount bgcolor="transparent" pd="0">
      
@@ -97,7 +128,60 @@ export default function Index() {
              </Mainbox>
 
           </Titlelayout>
-        
+    <CModal title={title} ref={ref} mold="cust" onOk={onOk} width={424}>
+        <Form form={form}   preserve={false} labelCol={{span: 4}} labelAlign='left'>
+           <Form.Item label="紧急  ≥" >
+              <Space>
+                <Form.Item name={['1', 'LimitValueMin']} rules={rules}>
+                   <InputNumber min={0} style={{width: '100px'}} max={99} step={0.01} onChange={onChange} />
+                </Form.Item>
+                <strong>%</strong>
+                <Form.Item name={['1', 'LimitValueMax']} noStyle initialValue={100}>
+                   <InputNumber   type="hidden" style={{border: "none"}} />
+                </Form.Item>
+              </Space>  
+           </Form.Item>
+           <Form.Item label="严重"   >
+                      <Space>
+                         <Form.Item name={['2', 'LimitValueMin']} rules={rules}>
+                             <InputNumber min={0} style={{width: '100px'}} step={0.01} disabled />
+                         </Form.Item>
+                         <p>% ~</p>
+                         <Form.Item name={['2', 'LimitValueMax']} >
+                             <InputNumber min={0} style={{width: '100px'}} step={0.01} disabled />
+                         </Form.Item>
+                      </Space>
+           </Form.Item>
+           <Form.Item label="一般  ≤" shouldUpdate>
+            {
+             ({getFieldValue}) => {
+              let init = (getFieldValue(['2', 'LimitValueMax']) -0.02).toFixed(2)
+               
+               return (
+                <Space>
+                <Form.Item name={['3', 'LimitValueMax']} noStyle>
+                     <InputNumber   style={{width: '100px'}} min={0.01} max={init} onChange={onChangel} />
+                 </Form.Item>
+                 <strong>%</strong>
+                 <Form.Item name={['3', 'LimitValueMin']} noStyle initialValue={0}>
+                     <InputNumber type="hidden" style={{border: "none"}} />
+                 </Form.Item>                        
+                </Space>
+               )
+
+
+             }
+
+
+            }
+                     
+           </Form.Item>
+           <Form.Item >
+              <p style={{color: "#f00"}}>注：输入的值为已排放量占配额的百分比</p>
+           </Form.Item>
+        </Form>
+
+     </CModal>
     </Pagecount>
   )
 }
