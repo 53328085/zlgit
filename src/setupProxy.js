9cfbb5eed6134d7845cf6dd1079a61a1a10909c5
ybmq <ybmq@2021>
2024-06-25 13:23:01 +0800

    commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use( 
         createProxyMiddleware("/api/V1", {
             // target:   // https://nis.chint.com/ (正式)   // http://10.5.7.60:4141（测试） 
-           target: 'http://10.5.7.60:4141', // 王建 http://10.5.25.19:4155/
+           target: 'http://10.5.7.60:4155', // 王建 http://10.5.25.19:4155/
             changeOrigin : true,
             ws: true,
             pathRewrite: {
