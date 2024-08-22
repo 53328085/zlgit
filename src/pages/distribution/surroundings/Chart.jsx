da9156b2cfb619227116abc4085ca8465c326dfb
ybmq <ybmq@2021>
2024-08-22 13:23:17 +0800

    commit

---
+++ src/pages/distribution/surroundings/Chart.jsx
@@ -0,0 +1,32 @@
+import React,{useState, useEffect} from 'react'
+import moment from 'moment';
+import {Space,DatePicker} from 'antd'
+import {SerachButton} from '@com/useButton'
+import Ichart from "@com/useEcharts/Ichart"
+import Titlelayout from '@com/titlelayout' 
+import styled from "styled-components"
+const Wrap=styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    row-gap:16px;
+`
+export default function Chart(props) {
+const {title=""} =props
+const onChange=()=>{
+
+}
+const customHead = (
+    <Wrap>
+     <div>日期</div>   
+    <DatePicker onChange={onChange} />
+    <SerachButton/>
+    </Wrap>
+)
+  return (
+ 
+        <Titlelayout title={title} extra={customHead}>Chart</Titlelayout>
+
+    
+  )
+}
