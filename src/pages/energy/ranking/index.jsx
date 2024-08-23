1fd0057ed61e8a0cb251e6b018bfcd0d7b3524c0
张子怡 <ybzzy@chint.com>
2024-08-23 14:53:34 +0800

    对比分析

--- src/pages/energy/ranking/index.jsx
+++ src/pages/energy/ranking/index.jsx
@@ -1,16 +1,17 @@
-import React, {useMemo } from 'react'
-import {useOutletContext} from 'react-router-dom' 
+import React, { useMemo, useState, useEffect } from 'react'
+import { useOutletContext } from 'react-router-dom'
 import styled from 'styled-components';
- 
-import Ichart  from '@com/useEcharts/Ichart';
+
+import Ichart from '@com/useEcharts/Ichart';
 import { energyRanking } from '@api/api'
 import Pagecount from "@com/pagecontent";
-import Titlelayout from '@com/titlelayout'; 
-import {getTime} from '@com/usehandler'
-import {useRequest} from 'ahooks'
+import Titlelayout from '@com/titlelayout';
+import { getTime } from '@com/usehandler'
+import { useRequest } from 'ahooks'
+import { Select, Form } from 'antd'
 const Mainbox = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: 1040px 1fr;
   column-gap: 16px;
   flex: 1;
   .chart {
@@ -20,15 +21,23 @@ const Mainbox = styled.div`
     justify-content: center;
   }
 `
+const CustTitle = styled.div`
+  font-size: 14px;
+  color: #515151;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
 export default function Index() {
-  let {exparams} = useOutletContext() 
-  let {areaId, date, energytype, type, shiftNo,projectId} = exparams;
-  
-  const getQuery =  (areaId, date, energytype, type, shiftNo,projectId) => {    
-  
-    let f = [areaId, energytype, type, shiftNo, projectId].every(key => Number.isInteger(key)) && date 
-   
-    if(!f) return;
+  const { Option } = Select;
+  let { exparams, setCustview } = useOutletContext()
+  let { areaId, date, energytype, type, shiftNo, projectId } = exparams;
+
+  const getQuery = (areaId, date, energytype, type, shiftNo, projectId) => {
+
+    let f = [areaId, energytype, type, shiftNo, projectId].every(key => Number.isInteger(key)) && date
+
+    if (!f) return;
     let params = {
       projectId,
       areaId,
@@ -38,97 +47,146 @@ export default function Index() {
       energytype,
     }
     return energyRanking.Query(params).then(res => {
-      let {success, data} = res
-      if(success) {
+      let { success, data } = res
+      if (success) {
         return data.constructor == Object ? data : {}
       }
     })
 
   }
- const {data} = useRequest(() => getQuery(areaId, date, energytype, type, shiftNo,projectId), {
-   refreshDeps: [areaId, date, energytype, type, shiftNo,projectId]
- })
+  const { data } = useRequest(() => getQuery(areaId, date, energytype, type, shiftNo, projectId), {
+    refreshDeps: [areaId, date, energytype, type, shiftNo, projectId]
+  })
 
- const options = {
-  series: [{ type: "bar"}],  
-  grid:{
-    left: "0px",
-    right: "0",
-    top: "0px",
-    bottom: "35px",
-    containLabel: true,
-  },
-  legend: {
-    bottom: "5px",
-  },
-  xAxis: {
-    type: 'value',
-   
-  },
-  yAxis: {
-    type: 'category',
-    axisTick: {
-      show: false
-    },   
-  },
+  const options = {
+    series: [{ type: "bar" }],
+    grid: {
+      left: "0px",
+      right: "0",
+      top: "0px",
+      bottom: "35px",
+      containLabel: true,
+    },
+    legend: {
+      bottom: "5px",
+    },
+    xAxis: {
+      type: 'value',
 
-}
- const dataset= {
+    },
+    yAxis: {
+      type: 'category',
+      axisTick: {
+        show: false
+      },
+    },
+
+  }
+  const dataset = {
     dimensions: [
-      {name: 'name'},
-      {name:  'value', displayName: ['','用电量(kWh)', 'm³', 'm³'][exparams?.energytype] },
-      
+      { name: 'name' },
+      { name: 'value', displayName: ['', '用电量(kWh)', 'm³', 'm³'][exparams?.energytype] },
+
     ]
   }
-let [lineopt, buildopt, roomopt] =useMemo(() => {
-let {line=[], building=[], room=[]} = data || {}
-line.sort((a, b) => parseFloat(a.value) -parseFloat(b.value) ) 
-building.sort((a, b) => parseFloat(a.value) -parseFloat(b.value) ) 
-room.sort((a, b) => parseFloat(a.value) -parseFloat(b.value) ) 
-return [{
-  ...options,
-  dataset: {
-    ...dataset,
-    source: line,
-  }
-},
-{
-  ...options,
-  dataset: {
-    ...dataset,
-    source: building,
-  }
-},
-{
-  ...options,
-  dataset: {
-    ...dataset,
-    source: room,
-  }
-}
+  let [lineopt, buildopt, roomopt] = useMemo(() => {
+    let { line = [], building = [], room = [] } = data || {}
+    line.sort((a, b) => parseFloat(a.value) - parseFloat(b.value))
+    building.sort((a, b) => parseFloat(a.value) - parseFloat(b.value))
+    room.sort((a, b) => parseFloat(a.value) - parseFloat(b.value))
+    return [{
+      ...options,
+      dataset: {
+        ...dataset,
+        source: line,
+      }
+    },
+    {
+      ...options,
+      dataset: {
+        ...dataset,
+        source: building,
+      }
+    },
+    {
+      ...options,
+      dataset: {
+        ...dataset,
+        source: room,
+      }
+    }
 
 
-]
-},[data])
- 
+    ]
+  }, [data])
+
+  const [rankingWay, setRankingWay] = useState(0)
+  // const custoption = [departmentOpt, workshopOpt][rankingWay]
+  const changeRankingWay = val => {
+    setRankingWay(val)
+  }
+  const areaRanking = (
+    <CustTitle className="title">按区域排名
+      <Select
+        size="middle"
+        style={{ width: '126px' }}
+        defaultValue={rankingWay}
+        onChange={changeRankingWay}
+      >
+        <Option value={0}>全部时段</Option>
+        <Option value={1}>上班时段</Option>
+        <Option value={2}>下班时段</Option>
+
+      </Select>
+    </CustTitle>
+  )
+  const [sortSelected, setSortSelected] = useState(1)
+  const sortOptions = [{
+    value: 1,
+    label: '升序'
+  }, {
+    value: 2,
+    label: '降序'
+  }]
+  const handleSortChange = (val) => {
+    setSortSelected(val)
+  }
+  const CustView = (
+    <Form layout="inline">
+      <Form.Item name="sort">
+        <Select options={sortOptions} defaultValue={sortSelected} style={{ width: 188, marginLeft: 16 }} onChange={handleSortChange}></Select>
+      </Form.Item>
+    </Form>
+
+  )
+
+  useEffect(() => {
+    setCustview(CustView);
+    return () => {
+      setCustview(undefined)
+    }
+  }, [])
+
   return (
     <Pagecount bgcolor="transparent" pd="0">
       <Mainbox>
-        <Titlelayout title="按回路排名" layout="flex">
-           <div className='chart'>
-              <Ichart {...lineopt} />
-           </div>
+        {/* 按回路排名 */}
+        <Titlelayout title={areaRanking} layout="flex">
+          <div className='chart'>
+            <Ichart {...lineopt} />
+          </div>
         </Titlelayout>
-        <Titlelayout title="按建筑排名" layout="flex">
+        {/* 按建筑排名 */}
+        <Titlelayout title="按能耗增量排名" layout="flex">
           <div className="chart">
-           <Ichart {...buildopt} />
-           </div>
+            <Ichart {...buildopt} />
+          </div>
         </Titlelayout>
-        <Titlelayout title="按房间排名" layout="flex">
-        <div className="chart">
-           <Ichart {...roomopt} />
-           </div>
-        </Titlelayout>
+        {/* <Titlelayout title="按房间排名" layout="flex">
+          <div className="chart">
+            <Ichart {...roomopt} />
+          </div>
+        </Titlelayout> */}
       </Mainbox>
     </Pagecount>
   )
--- src/pages/monitoring/analyse/index.js
+++ src/pages/monitoring/analyse/index.js
@@ -11,6 +11,7 @@ import { Monitoring } from '@api/api.js'
 
 import Mask from '@com/mask.jsx'
 import UseTransfer from './transfer';
+import { drawEcharts } from "@com/useEcharts"
 const { RangePicker } = DatePicker;
 const disabledDate = (current) => {
   return current && current > moment().endOf('day');
@@ -34,8 +35,9 @@ export default function Index() {
   const projectId = useSelector(state => state.system.menus.projectId)
   const [form] = Form.useForm();
   const [typeSelected, setTypeSelected] = useState()
-  const [baseLine, setBaseLine] = useState()
+  const [baseLine, setBaseLine] = useState([])
   const [unit, setUnit] = useState('单位')
+  const [baseLineValue, setBaseLineValue] = useState(); // 初始化状态
   const comparisonType = [
     {
       value: 1,
@@ -67,14 +69,17 @@ export default function Index() {
   }
   const onChangeBaseLine = (val) => {
     setBaseLine(val)
+    console.log(baseLine, val);
   }
 
+  const onChangeBaseLineValue = (newValue) => {
+    setBaseLineValue(newValue); // 更新状态
+  };
   const onChangeTime = (val) => {
     // setBaseLine(val)
   }
 
-
-  const [transTag, setTransTag] = useState('')
+  const [transTag, setTransTag] = useState(false)
   const [transferTitle, setTransferTitle] = useState({})
 
   const [subTable, setSubTable] = useState([])
@@ -101,7 +106,7 @@ export default function Index() {
     }
   ]
 
-  const settingClick = async () => {
+  const onSetDevices = async () => {
     setTransferTitle({
       subTitle: '选择需要对比的设备',
       unknownTitle: '未选中的设备',
@@ -147,6 +152,114 @@ export default function Index() {
     setTransTag(false)
     setTimeout(() => { setDeleteDom(false) }, 600)
   }
+  const analysisRef = useRef(null)
+  const onSetcontrast = () => {
+    console.log(baseLine, '2222baseLine');
+    if (!typeSelected) {
+      return message.error('请选择对比数据')
+    } else {
+      console.log(baseLine, '1111baseLine', baseLineValue);
+      if (baseLine.length != 0) {
+        if (baseLineValue == undefined) {
+          return message.error('请填写对比基准线具体数值')
+        }
+      }
+    }
+    if (baseLine.length != 0 && baseLineValue != undefined) {
+      drawEcharts(analysisRef.current, {
+        dataset: datasetContrast,
+        series: [{
+          type: "line", areaStyle: null, showSymbol: true,
+          markLine: {
+            data: [
+              //第一条线
+              {
+                yAxis: `${baseLineValue}`,
+                lineStyle: {
+                  type: "line",
+                  color: "red",
+                  width: 2,
+                },
+                label: {
+                  show: true,// 确保标签显示设置为true
+                  //将警示值放在哪个位置，三个值“start”,"middle","end"  开始  中点 结束
+                  position: "start",
+                  color: "white",
+                  backgroundColor: 'red',
+                  fontSize: 12,
+                  padding: 5, // 文字内边距
+                  lineHeight: 20, // 文字行高
+                  formatter: `${baseLineValue}` + `${unit}`,
+                },
+              },
+            ],
+          }
+        }, {
+          type: "line", areaStyle: null, showSymbol: true,
+        }],
+        grid: {
+          top: '30px',
+          left: 30,
+          right: 0,
+          bottom: '30px',
+          containLabel: true,
+        },
+        legend: {
+          top: 0,
+          icon: 'rect',
+          itemHeight: 2,
+          itemWidth: 12,
+          itemGap: 20,
+        }
+      })
+    } else {
+      drawEcharts(analysisRef.current, {
+        dataset: datasetContrast,
+        series: [{
+          type: "line", areaStyle: null, showSymbol: true,
+        }, {
+          type: "line", areaStyle: null, showSymbol: true,
+        }],
+        grid: {
+          top: '30px',
+          left: 0,
+          right: 0,
+          bottom: '30px',
+          containLabel: true,
+        },
+        legend: {
+          top: 0,
+          icon: 'rect',
+          itemHeight: 2,
+          itemWidth: 12,
+          itemGap: 20,
+        }
+      })
+    }
+
+
+  }
+  const datasetContrast = {
+    dimensions: ["time", "实时辐射", "实时功率"],
+    source: [
+      { time: "1", "实时辐射": 5600, "实时功率": 9600 },
+      { time: "2", "实时辐射": 4600, "实时功率": 3644 },
+      { time: "3", "实时辐射": 3600, "实时功率": 4644 },
+      { time: "4", "实时辐射": 5611, "实时功率": 9655 },
+      { time: "5", "实时辐射": 5644, "实时功率": 3677 },
+      { time: "6", "实时辐射": 4677, "实时功率": 3633 },
+      { time: "7", "实时辐射": 3688, "实时功率": 4655 },
+      { time: "8", "实时辐射": 5088, "实时功率": 2644 },
+      { time: "9", "实时辐射": 6677, "实时功率": 2641 },
+      { time: "10", "实时辐射": 5866, "实时功率": 5641 },
+      { time: "11", "实时辐射": 4677, "实时功率": 7645 },
+      { time: "12", "实时辐射": 1877, "实时功率": 2645 },
+    ],
+  };
+  useEffect(() => {
+
+
+  }, [])
   return (
 
     <Titlelayout title='对比分析'>
@@ -167,7 +280,7 @@ export default function Index() {
       >
         <Space size={64} split={<Cdivider />}  >
           <Form.Item name="button" style={{ marginBottom: 0 }}>
-            <Button type='primary' ghost onClick={() => settingClick()}>请选择要对比的设备</Button>
+            <Button type='primary' ghost onClick={() => onSetDevices()}>请选择要对比的设备</Button>
           </Form.Item>
           <Form.Item label="选择对比数据" name="type" style={{ marginBottom: 0 }}>
             <Select placeholder='请选择对比项目' options={comparisonType} defaultValue={typeSelected} style={{ width: 188, marginLeft: 16 }} onChange={onChangeType}></Select>
@@ -175,10 +288,10 @@ export default function Index() {
           <Form.Item name="line" style={{ marginBottom: 0 }} >
             <div style={{ display: 'flex', alignItems: 'center' }}>
               <Checkbox.Group value={baseLine} onChange={onChangeBaseLine}>
-                <Checkbox value="A">对比基准线</Checkbox>
+                <Checkbox value="selected">对比基准线</Checkbox>
               </Checkbox.Group>
               <div style={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
-                <InputNumber style={{ width: '100%' }} />
+                <InputNumber style={{ width: '100%' }} value={baseLineValue} onChange={onChangeBaseLineValue} />
                 <p style={{
                   width: '60px', textAlign: 'center',
                   height: '30px', lineHeight: '30px', background: '#f2f2f2',
@@ -191,7 +304,7 @@ export default function Index() {
           </Form.Item>
           <Form.Item label="请选择日期范围" name="time" style={{ marginBottom: 0 }}>
             <RangePicker onChange={onChangeTime} format="YYYY-MM-DD" style={{ width: '320px' }} disabledDate={disabledDate} />
-            <Button type="primary" style={{ marginLeft: '16px' }}>对比分析</Button>
+            <Button type="primary" style={{ marginLeft: '16px' }} onClick={() => onSetcontrast()}>对比分析</Button>
           </Form.Item >
         </Space>
       </Form>
@@ -199,6 +312,9 @@ export default function Index() {
         <UseTransfer transferTitle={transferTitle} columns={columns} subTable={subTable} unknownTable={unknownTable} saveValue={getSaveValue} closeValue={getCloseValue}></UseTransfer>
 
       </Mask>
+      <div style={{ width: "100%", marginTop: '16px', height: '90%' }} ref={analysisRef}>
+
+      </div>
     </Titlelayout>
 
   )
