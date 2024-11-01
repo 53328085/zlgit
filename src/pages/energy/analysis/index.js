e4b2f423af0de474a1b700362718e31edd0e88bf
张子怡 <ybzzy@chint.com>
2024-11-01 13:26:17 +0800

    能源管理-损耗分析bug解决

--- src/pages/energy/analysis/index.js
+++ src/pages/energy/analysis/index.js
@@ -1,17 +1,17 @@
-import React, {useState, useRef, useEffect, useCallback, memo} from 'react'
+import React, { useState, useRef, useEffect, useCallback, memo } from 'react'
 import { useAntdTable } from 'ahooks';
 import styled from 'styled-components';
-import {  Space } from 'antd';
+import { Space } from 'antd';
 
-import {useOutletContext} from 'react-router-dom' 
-import columns,  { onDesc} from './columns';
+import { useOutletContext } from 'react-router-dom'
+import columns, { onDesc } from './columns';
 import UserTable from "@com/useTable";
-import { EnergyLossRuntime} from '@api/api.js'
-import Titlelayout from '@com/titlelayout' 
+import { EnergyLossRuntime } from '@api/api.js'
+import Titlelayout from '@com/titlelayout'
 import Pagecount from "@com/pagecontent";
 import UserTree from "@com/useTree"
-import {  ExportExcel} from '@com/useButton'
-import {getTime} from '@com/usehandler'
+import { ExportExcel } from '@com/useButton'
+import { getTime } from '@com/usehandler'
 const Mainbox = styled.div`
    display: grid;
    flex: 1;
@@ -26,81 +26,83 @@ const Mainbox = styled.div`
 
 export default function Index() {
   const tbref = useRef()
-  
-  let {exparams, setCustview} = useOutletContext()
-  let {areaId, projectId, type, date, energytype, shiftNo} = exparams
-  const  pageTotal = useRef()
- 
- 
+
+  let { exparams, setCustview } = useOutletContext()
+  let { areaId, projectId, type, date, energytype, shiftNo } = exparams
+  console.log(exparams, "---exparams")
+  const pageTotal = useRef()
+
+
   const [treeId, setTreeId] = useState(null)
   const [line, setLine] = useState(null)
-  const {tree, setTree} =useState(null)
-  const { queryByLine, queryByBuilding } = EnergyLossRuntime 
-  const CustView =(
+  const { tree, setTree } = useState(null)
+  const { queryByLine, queryByBuilding } = EnergyLossRuntime
+  const CustView = (
     <Space size={16}>
       <ExportExcel tb={tbref} />
-     </Space>
-    )
-    useEffect(() => {
-      setCustview(CustView);
-      return () => {
-        setCustview(undefined)
-      }
-     }, []) 
-  const getTableData = ({current, pageSize}) => {
-      // 0 建筑 1线路
-   
-    if(Object.values(exparams)?.length < 6) return;
-    if(!Array.isArray(treeId) || !isFinite(line)) return   
+    </Space>
+  )
+  useEffect(() => {
+    setCustview(CustView);
+    return () => {
+      setCustview(undefined)
+    }
+  }, [])
+  const getTableData = ({ current, pageSize }) => {
+    // 0 建筑 1线路
+
+    if (Object.values(exparams)?.length < 6) return;
+    if (!Array.isArray(treeId) || !isFinite(line)) return
     let time = getTime(date, type)
     let params = {
       projectId,
       areaId,
       shiftId: shiftNo,
-      energyType:energytype ,
+      energyType: energytype,
       type,
       date: time,
       selectIds: treeId
     }
+    console.log(params, "---params")
     const index = Number(line)
     const handler = [queryByBuilding, queryByLine][index]
     return handler(current, pageSize, params).then(res => {
-      let {success, data, total=0} = res 
-        pageTotal.current = total
-      if(success){
-         return {
-           list: Array.isArray(data) ? data : [],
-           total: Array.isArray(data) ? total : 0,
-         }
-      }else{
+      let { success, data, total = 0 } = res
+      pageTotal.current = total
+      if (success) {
+        return {
+          list: Array.isArray(data) ? data : [],
+          total: Array.isArray(data) ? total : 0,
+        }
+      } else {
         return {
           list: [],
           total: 0
         }
-      
+
       }
     })
   }
-  const {tableProps} = useAntdTable(getTableData, {
-    defaultParams: [{current: 1, pageSize: 20}],
-    refreshDeps: [exparams, treeId,   line]
+  const { tableProps } = useAntdTable(getTableData, {
+    defaultParams: [{ current: 1, pageSize: 20 }],
+    refreshDeps: [exparams, treeId, line]
   })
- 
-  const onExport =useCallback(() => {   
-   
-    return  getTableData({current: 1, pageSize: pageTotal.current})
- }, [exparams, treeId,   line])
+
+  const onExport = useCallback(() => {
+
+    return getTableData({ current: 1, pageSize: pageTotal.current })
+  }, [exparams, treeId, line])
 
   return (
     <Pagecount pd="0">
-       <Mainbox>
-           <UserTree areaId={areaId}   setTreeId={setTreeId} setLine={setLine}     /> 
+      <Mainbox>
+        <UserTree areaId={areaId} setTreeId={setTreeId} setLine={setLine} energytype={energytype} />
         <Titlelayout title="损耗分析" layout="flex">
-           <div className='tablebox'>
-              <UserTable ref={tbref} size='small'  {...tableProps}   sheetName="损耗分析表" onExport={onExport} columns={columns} rowKey='Id' />
-           </div>
+          <div className='tablebox'>
+            <UserTable ref={tbref} size='small'  {...tableProps} sheetName="损耗分析表" onExport={onExport} columns={columns} rowKey='Id' />
+          </div>
         </Titlelayout>
-        </Mainbox>
+      </Mainbox>
     </Pagecount>
   )
 }
