5a8cc4c6c59fffb81de3958c68b52c79f5bd4adc
53328085 <53328085@qq.com>
2024-12-16 08:36:28 +0800

    主题色修改

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         createProxyMiddleware("/api/V1", {
             // target:   // https://nis.chint.com/ (正式)   // http://10.5.7.60:4155（测试） 
-            target: 'http://10.5.7.60:4155',
+            target: 'https://nis.chint.com/',
             // target: 'http://10.5.25.7:4155',
             changeOrigin: true,
             ws: true,
