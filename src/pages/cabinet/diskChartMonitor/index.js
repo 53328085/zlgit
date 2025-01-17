47fd2ea55cb51b607f068a105f4990e199b696cd
ybgsh <ybgsh@chint.com>
2025-01-17 09:54:10 +0800

    样式

--- src/pages/cabinet/diskChartMonitor/index.js
+++ src/pages/cabinet/diskChartMonitor/index.js
@@ -3,21 +3,8 @@ import simple from './imgs/simple.svg'
 import styled from "styled-components";
 
 export default function Index(){
-
-    const MainPage = styled.div`
-        height: 857px;
-        width: 100%;
-        overflow-y: hidden;
-    `
-
+    
     return(
-        <MainPage>
-            <div >
-            <img src={simple} style={{width:'260px', height:'800px'}} ></img>
-            </div>
-            <div>
-            <img src={simple} style={{width:'260px', height:'800px'}}></img>
-            </div>
-        </MainPage>
+        <div>盘面图</div>
     )
 }
