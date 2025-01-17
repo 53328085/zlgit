3e64eb837e7a35d492ccc71c8bbbb904d2d27ff0
ybgsh <ybgsh@chint.com>
2025-01-17 08:07:16 +0800

    123

--- src/pages/cabinet/diskChartMonitor/index.js
+++ src/pages/cabinet/diskChartMonitor/index.js
@@ -1,7 +1,23 @@
 import React, {useState, useEffect} from 'react'
+import simple from './imgs/simple.svg'
+import styled from "styled-components";
 
 export default function Index(){
+
+    const MainPage = styled.div`
+        height: 857px;
+        width: 100%;
+        overflow-y: hidden;
+    `
+
     return(
-        <div>盘面图监控</div>
+        <MainPage>
+            <div >
+            <img src={simple} style={{width:'260px', height:'800px'}} ></img>
+            </div>
+            <div>
+            <img src={simple} style={{width:'260px', height:'800px'}}></img>
+            </div>
+        </MainPage>
     )
 }
