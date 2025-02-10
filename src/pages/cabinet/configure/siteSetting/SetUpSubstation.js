9a0a583d7f10c34abf8be6228dce3c1f1cf64038
zzy <ybzzy@chint.com>
2025-02-10 09:43:42 +0800

    代码提交

--- src/pages/cabinet/configure/siteSetting/SetUpSubstation.js
+++ src/pages/cabinet/configure/siteSetting/SetUpSubstation.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import styled, { css } from "styled-components";
-import { Form, Select, Input, message, Drawer } from 'antd'
+import { Form, Select, Input, InputNumber, message, Drawer } from 'antd'
 
 import Cupload from "@com/useUpload.js"
 
@@ -46,23 +46,23 @@ export default function Index() {
         <Content>
             <Form form={form} layout="vertical" className='form'>
                 <div className='left'>
-                    <Item label="变电站名称" name="creditCode" rules={rules}  >
-                        <Input style={{ width: 420, height: 40 }}></Input>
+                    <Item label="变电站名称" name="name" rules={rules}  >
+                        <Input style={{ width: 420, height: 40 }} />
                     </Item>
-                    <Item label="变电站地址" name="creditCode" rules={rules}  >
-                        <Input style={{ width: 420, height: 40 }}></Input>
+                    <Item label="变电站地址" name="adress" rules={rules}  >
+                        <Input style={{ width: 420, height: 40 }} />
                     </Item>
                     <div className='flex'>
-                        <Item label="变电站容量(kVA)" name="creditCode" rules={rules}  >
-                            <Input style={{ width: 180, height: 40 }}></Input>
+                        <Item label="变电站容量(kVA)" name="rong" rules={rules}  >
+                            <InputNumber style={{ width: 180, height: 40, lineHeight: '40px' }} min='0' />
                         </Item>
-                        <Item label="变电站电压等级(kV)" name="creditCode" rules={rules}  >
-                            <Input style={{ width: 180, height: 40 }}></Input>
+                        <Item label="变电站电压等级(kV)" name="grade" rules={rules}  >
+                            <InputNumber style={{ width: 180, height: 40, lineHeight: '40px' }} min='0' />
                         </Item>
                     </div>
                 </div>
                 <div className='right'>
-                    <Item label="变电站图片上传" name="creditCode" rules={rules} >
+                    <Item label="变电站图片上传" name="image" rules={rules} >
                         {/* value={energyImage.current} onChange={onChnage} */}
                         <div className='imgbox'>
                             <Cupload wpx={1368} hpx={800} swpx={400} shpx={235} maximum={800} />
