b752633d798bed0c562f9a8d108aea24bd8423f4
zzy <ybzzy@chint.com>
2025-02-14 13:39:22 +0800

    智能配电柜环境监控、碳排统计接口对接

--- src/pages/cabinet/carbonStatistics/index.js
+++ src/pages/cabinet/carbonStatistics/index.js
@@ -6,7 +6,7 @@ import Card from './card'
 import { isObject } from "@com/usehandler"
 import { CustTransO, i18t, i18warning } from "@com/useButton"
 import styled, { css } from 'styled-components'
-
+import { Cabinets } from '@api/api.js'
 import { Radiogroup, Cdivider } from "@com/comstyled"
 import { enterprise, selectProjectId, adaptation } from "@redux/systemconfig"
 import Titlelayout from '@com/titlelayout';
@@ -56,7 +56,7 @@ const Mainbox = styled.div`
      grid-template-columns:656px 544px 448px;
      column-gap: 16px;
      .imgbox {
-      background-color: ${props => props.theme.primaryderived};
+      //background-color: ${props => props.theme.primaryderived};
       display: flex;
       align-items: center;
     //  height:100%;
@@ -109,11 +109,11 @@ const Mainbox = styled.div`
 const options = [
     {
         label: i18t("comm", "month"),
-        value: 1,
+        value: 2,
     },
     {
         label: i18t("comm", "year"),
-        value: 2,
+        value: 3,
     },
 ];
 
@@ -125,88 +125,101 @@ const options = [
 export default function Index() {
     const { enterpriseId } = useSelector(enterprise)
     let { laptop } = useSelector(adaptation)
-    // 概览数据查询
-    let Quota = {}
-    const { isSuccess, data: quotaData } = useOverviewQuery(enterpriseId, {
-        skip: !Number.isInteger(enterpriseId)
-    })
-    if (isSuccess) {
-        Quota = quotaData?.data ?? {}
-    }
 
 
     // 获取园区图片
-
+    const [Quota, setQuota] = useState([])
+    const [CarbonRanking, setCarbonRanking] = useState([])
     const projectId = useSelector(selectProjectId)
     let projectImg = useRef()
-
-    const { isSuccess: imgsuc, data: imgData, refetch } = useProjectPhotoQuery(projectId, {
-        skip: !Number.isInteger(projectId),
-        refetchOnMountOrArgChange: true,
-    })
-
-    if (imgsuc) {
-        projectImg.current = imgData ?? null
-    }
-
-
-
-
-
-    // 碳排放排名
-    const dataSource = useRef([])
-
-    const columnstable = [
-        {
-            title: i18t("comm", "index"),
-            width: 80,
-            render: (text, record, index) => `${index + 1}`
-        },
-        { title: i18t("cabinet", "CircuitName"), dataIndex: 'name', key: 'name', align: "center", },
-        { title: i18t("cabinet", "CarbonEmission", { param: '(tCO₂)' }), dataIndex: 'value', key: 'value', align: "center", },
-        { title: i18t("comm", "ratio"), dataIndex: 'proportion', key: 'proportion', align: "center", },
-    ]
-
-    const [rankparam, setRankParam] = useState({ enterpriseId, type: 1 })
-    const { isSuccess: ranksuc, data: rankData } = useRankingQuery(rankparam, {
-        skip: !Number.isInteger(rankparam.enterpriseId)
-    })
-
-    if (ranksuc) {
-        let { success, data, errMsg } = rankData
-        if (success && Array.isArray(data)) {
-            dataSource.current = data
-
-        } else {
-            if (!success) i18warning(errMsg)
-            dataSource.current = data
-        }
-
-    } else {
-        dataSource.current = []
-    }
-
-
+    const [rankparam, setRankParam] = useState({ enterpriseId, type: 2 })
+    const [ratioparm, setRatioparm] = useState({ enterpriseId, type: 2 })
 
     // 月度碳排放
     const [montharg, setMontharg] = useState({
         enterpriseId,
-        type: 1,
+        type: 2,
     })
 
-    const monthchange = (e) => {
-        setMontharg({
-            ...montharg,
-            type: e.target.value
-        })
+    const QuerySiteCarbonOverview = async () => {
+        try {
+            const { success, data, errMsg } = await Cabinets.QuerySiteCarbonOverview(1)
+            if (success && data) {
+                setQuota(data)
+            } else {
+                setQuota([])
+                if (!success) message.warning(errMsg)
+            }
+        } catch (error) {
+
+        }
+
     }
-    const { isSuccess: msuc, data: monthData } = useMonthQuery(montharg, {
-        skip: !Number.isInteger(montharg.enterpriseId)
-    })
+    const QueryLineCarbonRanking = async () => {
+        try {
+            const { success, data, errMsg } = await Cabinets.QueryLineCarbonRanking(1, rankparam.type)
+            if (success && data) {
+                setCarbonRanking(data)
+            } else {
+                setCarbonRanking([])
+                if (!success) message.warning(errMsg)
+            }
+        } catch (error) {
 
-    let moption = {
-        color: ["#ff7345", "#6a6e88"],
-        series: [{ type: "bar", stack: "total", seriesLayoutBy: 'row' }, { type: "bar", stack: "total", seriesLayoutBy: 'row' }],
+        }
+
+    }
+
+
+    // 碳排占比
+    const [poptions, setPoptions] = useState({
+        type: 3,
+        pieData: { data: [], total: "100%", radius: ["50%", "65%"], center: ['50%', '50%'] },
+        legend: {
+            bottom: 5,
+            top: 'auto',
+
+        },
+    })
+    const QueryLineCarbonRankingPie = async () => {
+        try {
+            const { success, data, errMsg } = await Cabinets.QueryLineCarbonRanking(1, ratioparm.type)
+            if (success && data) {
+                if (success && Array.isArray(data) && data.length > 0) {
+                    data.forEach(item => {
+                        item.name = item.lineName;
+                        item.value = item.lineCarbonEmission;
+                    });
+                    setPoptions({
+                        ...poptions,
+                        pieData: {
+                            ...poptions.pieData,
+                            data,
+                        }
+                    })
+                    console.log(poptions)
+                } else {
+                    if (!success) i18warning(errMsg)
+                    setPoptions({
+                        ...poptions,
+                        pieData: {
+                            ...poptions.pieData,
+                            data: [],
+                        }
+                    })
+                }
+            } else {
+                if (!success) message.warning(errMsg)
+            }
+        } catch (error) {
+
+        }
+
+    }
+
+    const [moption, setMoption] = useState({
+        color: ["#ff7345"],
+        series: [{ type: "bar", seriesLayoutBy: 'row' }],
         grid: {
             right: 0,
             left: 0,
@@ -217,105 +230,74 @@ export default function Index() {
         legend: {
             top: "0px",
             icon: 'rect',
-            right: "32px"
-        },
-        dataset: {
-        }
-    }
-    if (msuc) {
-        let { x = [], y = [], y1 = [] } = monthData?.data ?? {}
-        moption.dataset = {
-            dimensions: [
+            right: "32px",
+            data: [
                 { name: i18t("comm", "time"), type: "time" },
-                { name: i18t("cabinet", "directe", { param: '' }) },
-                { name: i18t("cabinet", "indirecte", { param: '' }) },
-            ],
-            sourceHeader: false,
-            source: [x, y, y1]
+                { name: i18t("cabinet", "CarbonEmission", { param: '(tCO₂)' }) },
+            ]
+
+        }, dataset: {}
+    })
+
+    const QuerySiteCarbonGraph = async () => {
+        try {
+            const { success, data, errMsg } = await Cabinets.QuerySiteCarbonGraph(1, montharg.type)
+            console.log(success, data)
+
+            if (success && data) {
+                let { x, y } = data
+                console.log(x, y)
+                let dataset = {
+                    dimensions: [
+                        { name: i18t("comm", "time"), type: "time" },
+                        { name: i18t("cabinet", "CarbonEmission", { param: '(tCO₂)' }) },
+                        // { name: i18t("cabinet", "directe", { param: '' }) },
+                        // { name: i18t("cabinet", "indirecte", { param: '' }) },
+                    ],
+                    source: [
+                        x,
+                        y,
+                    ],
+                    sourceHeader: false,
+                }
+
+                setMoption({
+                    ...moption,
+                    dataset
+                })
+            } else {
+                if (!success) i18warning(errMsg)
+                setMoption({
+                    ...moption
+                })
+            }
+
+        } catch (error) {
+
         }
 
     }
-    // 碳排占比
-    const poptions = useRef({
-        type: 3,
-        pieData: { data: [], total: "100%", radius: ["50%", "65%"], center: ['50%', '50%'] },
-        legend: {
-            bottom: 5,
-            top: 'auto',
 
+
+    // 碳排放排名
+
+    const columnstable = [
+        {
+            title: i18t("comm", "index"),
+            width: 80,
+            render: (text, record, index) => `${index + 1}`
         },
-
-    })
-    const [ratioparm, setRatioparm] = useState({ enterpriseId, type: 1 })
-
-    const { isSuccess: ratiosuc, data: ratiodata } = useRatioQuery(ratioparm, {
-        skip: !Number.isInteger(ratioparm.enterpriseId),
-    })
-
-    if (ratiosuc) {
-        let { success, data, errMsg } = ratiodata
-        if (success && Array.isArray(data) && data.length > 0) {
-            poptions.current = ({
-                ...poptions.current,
-                pieData: {
-                    ...poptions.current.pieData,
-                    data,
-                }
-            })
-
-        } else {
-            if (!success) i18warning(errMsg)
-            poptions.current = ({
-                ...poptions.current,
-                pieData: {
-                    ...poptions.current.pieData,
-                    data: [],
-                }
-            })
-        }
-
-    }
+        { title: i18t("cabinet", "CircuitName"), dataIndex: 'lineName', key: 'lineName', align: "center", },
+        { title: i18t("cabinet", "CarbonEmission", { param: '(tCO₂)' }), dataIndex: 'lineCarbonEmission', key: 'lineCarbonEmission', align: "center", },
+        { title: i18t("comm", "ratio"), dataIndex: 'lineCarbonEmissionRatio', key: 'lineCarbonEmissionRatio', align: "center", },
+    ]
 
 
-    // 分类能耗
-    const [classarg, setClassarg] = useState({
-        enterpriseId,
-        type: 1,
-    })
-    // let ClassEData = []
-    const classChange = (e) => {
-        setClassarg({
-            ...classarg,
-            type: e.target.value
-        })
-    }
-    const { isSuccess: classSuc, data: classData } = useEnergyQuery(classarg, {
-        skip: !Number.isInteger(classarg.enterpriseId)
-    })
-    // if (classSuc) {
-    //     ClassEData = classData?.data ?? []
-    // }
-
-
-
-
-    /*  const Ctitle = (
-      <div className='custTitle'>
-         <span>实时碳排放(tCO₂)</span>
-         <Radiogroup options={options}
-                      defaultValue={1}
-                      onChange={(e) => {
-                        onGetRealData(e)
-                      }} 
-                      optionType="button"
-                      buttonStyle="solid" /> 
-      </div>
-     ) */
     const Rtitle = (
         <div className='custTitle'>
             <span><CustTransO text="RankingOfCarbon" ns="cabinet" /></span>
             <Radiogroup options={options}
-                defaultValue={1}
+                defaultValue={2}
                 onChange={(e) => setRankParam({ ...rankparam, type: e.target.value })}
                 optionType="button"
                 buttonStyle="solid" />
@@ -326,7 +308,7 @@ export default function Index() {
         <div className='custTitle'>
             <span><CustTransO text="ProportionOfCarbonEmissions" ns="cabinet" /></span>
             <Radiogroup options={options}
-                defaultValue={1}
+                defaultValue={2}
                 onChange={(e) => {
                     setRatioparm({ ...ratioparm, type: e.target.value })
                 }}
@@ -334,80 +316,54 @@ export default function Index() {
                 buttonStyle="solid" />
         </div>
     )
-    // const CItitle = (
-    //     <div className='custTitle'>
-    //         <span><CustTransO text="Classificationratio" ns="cabinet" />（tCO₂）</span>
-    //         <Radiogroup options={options}
-    //             defaultValue={1}
-    //             onChange={classChange}
-    //             optionType="button"
-    //             buttonStyle="solid" />
-    //     </div>
-    // )
-
     const Mtitle = (
         <div className='custTitle'>
-            <span><CustTransO text="Classificationratio" ns="cabinet" />（tCO₂）</span>
+            <span><CustTransO text={montharg.type == 2 ? 'MonthlyCarbonEmissions' : 'AnnualCarbonEmissions'} ns="cabinet" />（tCO₂）</span>
             <Radiogroup options={options}
-                defaultValue={1}
-                onChange={monthchange}
+                defaultValue={2}
+                onChange={(e) => {
+                    setMontharg({
+                        ...montharg,
+                        type: e.target.value
+                    })
+                }}
                 optionType="button"
                 buttonStyle="solid" />
         </div>
     )
+    useEffect(() => {
+        QuerySiteCarbonOverview()
+    }, [])
 
-    // 文字滚动
-    // const [isScrolle, setIsScrolle] = useState(true);
-    const speed = 30;
-    const contentref = useRef()
-    const contentref2 = useRef()
-    const wrapref = useRef()
-    // const hoverHandler = (flag) => setIsScrolle(flag);
-
-
-    // useEffect(() => {
-    //     if (ClassEData?.length < 6) {
-    //         wrapref.current.scrollTop = 0
-    //         contentref2.current.style.display = "none"
-    //         contentref2.current.innerHTML = ''
-    //         return
-    //     }
-    //     contentref2.current.style.display = "flex"
-    //     contentref2.current.innerHTML = contentref.current.innerHTML;
-    //     let timer;
-    //     if (isScrolle) {
-    //         timer = setInterval(
-    //             () => wrapref.current.scrollTop >= contentref.current.scrollHeight ? (wrapref.current.scrollTop = 0) : wrapref.current.scrollTop++
-    //             ,
-    //             speed
-    //         );
-    //     }
-    //     return () => {
-    //         clearInterval(timer)
-    //     }
-
-    // }, [isScrolle, ClassEData])
-
+    useEffect(() => {
+        QueryLineCarbonRanking()
+    }, [rankparam])
+    useEffect(() => {
+        QueryLineCarbonRankingPie()
+    }, [ratioparm])
+    useEffect(() => {
+        QuerySiteCarbonGraph()
+    }, [montharg])
     return (
         <Pagecount bgcolor="#eeeff3" pd={0}>
             <Mainbox laptop={laptop}>
                 <div className='up' key="up" >
-                    <Card name={<CustTransO ns="cabinet" text="annualquota" param="(tCO₂)" />} laptop={laptop} title="" value={Quota.annualQuota} yoy={Quota.annualQuotaYoy} key="a" />
-                    <Card name={<CustTransO ns="cabinet" text="Annualee" param="(tCO₂)" />} laptop={laptop} title="" value={Quota.annualEmissionEquivalent} yoy={Quota.annualEmissionEquivalentYoy} key="b" />
-                    <Card title={<CustTransO ns="cabinet" text="Annualce" param="(tCO₂)" />} laptop={laptop} value={Quota.annualResidualCarbonEmission} yoy={Quota.annualResidualCarbonEmissionPercent} key="c" />
+                    <Card name={<CustTransO ns="cabinet" text="Annualquota" param="(tCO₂)" />} laptop={laptop} title="" value={Quota.annualQuota} yoy={Quota.annualQuotaYoy} key="a" />
+                    <Card name={<CustTransO ns="cabinet" text="Annualee" param="(tCO₂)" />} laptop={laptop} title="" value={Quota.annualEmission} yoy={Quota.annualEmissionYoy} key="b" />
+                    <Card title={<CustTransO ns="cabinet" text="Annualce" param="(tCO₂)" />} laptop={laptop} value={Quota.annualResidual} yoy={Quota.annualResidualPct} key="c" />
                     {/*  <Cdivider type="h" borderColor="#bcbcbc" /> */}
-                    <Card title={<CustTransO ns="cabinet" text="directe" param="(tCO₂)" />} laptop={laptop} value={Quota.directEmission} yoy={Quota.directEmissionPercent} key="d" />
-                    <Card title={<CustTransO ns="cabinet" text="indirecte" param="(tCO₂)" />} laptop={laptop} value={Quota.indirectEmission} yoy={Quota.indirectEmissionPercent} key="e" />
+                    <Card title={<CustTransO ns="cabinet" text="directe" param="(tCO₂)" />} laptop={laptop} value={Quota.dailyEmission} yoy={Quota.dailyEmissionPct} key="d" />
+                    <Card title={<CustTransO ns="cabinet" text="indirecte" param="(tCO₂)" />} laptop={laptop} value={Quota.monthlyEmission} yoy={Quota.monthlyEmissionPct} key="e" />
                 </div>
                 <div className='center' key="center">
                     <div className='imgbox'>
-                        <img src={projectImg.current || imgsrcs['cabinet']} alt="" />
+                        <img src={Quota.siteImage || imgsrcs['cabinet']} alt="" />
                     </div>
 
 
                     <Titlelayout title={Rtitle} key="rank">
                         <div className='chart'>
-                            <Table columns={columnstable} className="tablestyle" rowKey={(columns) => columns.key} dataSource={dataSource.current} scroll={{
+                            <Table columns={columnstable} className="tablestyle" rowKey={(columns) => columns.key} dataSource={CarbonRanking} scroll={{
                                 y: 300,
                             }} />
                         </div>
@@ -415,7 +371,7 @@ export default function Index() {
                     </Titlelayout>
                     <Titlelayout title={Raitle} layout="flex" >
                         <div className='chart'>
-                            <Ichart {...poptions.current} />
+                            <Ichart {...poptions} />
                         </div>
 
                     </Titlelayout>
