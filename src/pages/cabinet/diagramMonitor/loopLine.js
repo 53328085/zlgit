04f8999f676a22d47dea4d7c38bbcec43fa95c5d
ybgsh <ybgsh@chint.com>
2025-03-07 09:43:40 +0800

    123

--- src/pages/cabinet/diagramMonitor/loopLine.js
+++ src/pages/cabinet/diagramMonitor/loopLine.js
@@ -95,13 +95,13 @@ export default React.memo((props) => {
                 if(res && res.response.code == 0 && Array.isArray(res.response.data) && res.response.data.length >0){
                     let deviceData = res.response.data
                     deviceData.map(item => {
-                        if (item.DigitalInstatus1 == 1) {
+                        if (item.name == 'DigitalInstatus1' && item.value == "1") {
                             state.status = 'close'
                         }
-                        if (item.DigitalInstatus2 == 1) {
+                        if (item.name == 'DigitalInstatus2' && item.value == "1") {
                             state.status = 'open'
                         }
-                        if (item.DigitalInstatus3 == 1) {
+                        if (item.name == 'DigitalInstatus3' && item.value == "1") {
                             state.status = 'error'
                         }
         
