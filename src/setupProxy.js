fbb4ca226ad01e4a0762c9a86b6edf2eafe9c5db
ybgsh <ybgsh@chint.com>
2025-04-27 08:01:56 +0800

    123

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         createProxyMiddleware("/api/V1", {
             // target:   // https://nis.chint.com/ (正式)   // http://10.5.7.60:4155（测试） 
-            //target: 'https://nis.chint.com/',
+            // target: 'http://127.0.0.1:4155/',
             target: 'http://10.5.7.60:4155',
             changeOrigin: true,
             ws: true,
