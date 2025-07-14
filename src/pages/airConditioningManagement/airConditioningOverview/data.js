684dbe5cfa19cb16d369fa6102859bfca7e7e616
ybmq <ybmq@2021>
2025-07-14 09:30:54 +0800

    公共树优化，设备监控管理调整

---
+++ src/pages/airConditioningManagement/airConditioningOverview/data.js
@@ -0,0 +1,13 @@
+import moment from 'moment'
+export const Init_Value = {
+    dtype: "1",
+    date: moment(),
+  };
+  export const Date_Value = [
+    {
+      label: "日",
+      value: "1",
+    },
+    { label: "月", value: "2" },
+    { label: "年", value: "3" },
+  ];
