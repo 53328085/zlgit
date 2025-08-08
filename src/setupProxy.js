9d8e1c452732b5b358d297cafdb92ad678475409
ybgsh <ybgsh@YBGSH-BOOK>
2025-08-08 08:31:18 +0800

    测试服务

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
         createProxyMiddleware("/api/V1", {
             // target:   // https://nis.chint.com/ (正式)   // http://10.5.7.60:4155（测试） 
             // target: 'http://127.0.0.1:4155/',
-            target: 'http://10.5.7.60:4155/',
+            target: 'http://10.5.7.60:4155',
             changeOrigin: true,
             ws: true,
             pathRewrite: {
