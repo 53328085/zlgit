26f1a63d159a67af8390f5da6e4c276b36d764fc
53328085 <53328085@qq.com>
2025-10-13 18:51:57 +0800

    并网柜监控

---
+++ src/pages/photovoltaic/station/data.js
@@ -0,0 +1,16 @@
+export const states=[
+    {label:'全部',value:'0'},
+    {label:'离线',value:'1'},
+    {label:'在线',value:'2'},
+ 
+]
+export const  options = [
+    {
+      label: '图表模式',
+      value: '1',
+    },
+    {
+      label: '列表模式',
+      value: '2',
+    },
+  ];
--- src/pages/photovoltaic/station/style.js
+++ src/pages/photovoltaic/station/style.js
@@ -7,10 +7,10 @@ const borderBox = ({ borderRadius = "4px", bgcolor = "#fff" }) => css`
 
 export const getBackgroundColor = (theme) => {
   const color = theme?.bg || theme.cardHeadBg;
-  if (color && color !== '#ffffff') {
+  if (color && color !== "#ffffff") {
     return color;
   }
-  return 'rgba(229, 236, 245, 0.5)';
+  return "rgba(229, 236, 245, 0.5)";
 };
 export const Container = styled.div`
   display: grid;
@@ -19,203 +19,214 @@ export const Container = styled.div`
   gap: 16px;
 `;
 export const TopBox = styled.div`
-  
   flex: 1;
   display: grid;
   color: #515151;
-  grid-template-columns: 440px minmax(340px, 1fr) 570px 290px ;
+  grid-template-columns: 440px minmax(340px, 1fr) 570px 290px;
   gap: 16px;
   justify-content: flex-end;
-  .infoBox1{
-  display:flex;
-  .powerStation{
-    width: 160px;
-    height: 150px;
-  }
-.content{
-  display: grid;
-  align-items: center;
-  .info{
+  .infoBox1 {
     display: flex;
-    align-items: baseline;
-    span:nth-of-type(1){
-    width: 6px;
-    height: 6px;
-    background: #1E50E6;
-    display: inline-block;
-    border-radius: 50%;
-    margin-right: 10px;
+    .powerStation {
+      width: 160px;
+      height: 150px;
     }
- span:nth-of-type(2){
-    display: inline-block;
-    width: 70px;
-    color: #606266;
-}
-    span:nth-of-type(3){
-    display: inline-block;
-    width: 150px;
-    
+    .content {
+      display: grid;
+      align-items: center;
+      .info {
+        display: flex;
+        align-items: baseline;
+        span:nth-of-type(1) {
+          width: 6px;
+          height: 6px;
+          background: #1e50e6;
+          display: inline-block;
+          border-radius: 50%;
+          margin-right: 10px;
+        }
+        span:nth-of-type(2) {
+          display: inline-block;
+          width: 70px;
+          color: #606266;
+        }
+        span:nth-of-type(3) {
+          display: inline-block;
+          width: 150px;
+        }
+      }
     }
-}
   }
-  }
-  .powerNum{
+  .powerNum {
     height: 60px;
     margin-top: 10px;
     border-radius: 4px;
-    display:flex;
+    display: flex;
     justify-content: space-around;
     align-items: center;
-    
-    .numBox{
-    display:flex;
-    align-items: center;
-    color:#606266;
-  .powerIcon{
-    width: 36px;
-    height: 36px;
+
+    .numBox {
+      display: flex;
+      align-items: center;
+      color: #606266;
+      .powerIcon {
+        width: 36px;
+        height: 36px;
+      }
+      .num {
+        margin-left: 10px;
+        font-weight: 400;
+        font-size: 13px;
+        div:nth-of-type(1) {
+          font-size: 18px;
+          color: #1e50e6;
+          font-weight: 600;
+        }
+      }
+    }
+
+    background-color: ${(props) => getBackgroundColor(props.theme)};
   }
-    .num{
-    margin-left:10px;
-    font-weight: 400;
-    font-size: 13px;
-    div:nth-of-type(1){
-      font-size: 18px;
-      color: #1E50E6;
-      font-weight: 600;
-    }
-    }
-    }
-    
-    background-color:${props => getBackgroundColor(props.theme)};
-  }
- .infoBox2{
- display:flex;
- align-items: center;
- justify-content: space-around;
- .info{
-    display: grid;
-    gap: 16px;
-    color:#606266;
-    .box{
+  .infoBox2 {
     display: flex;
     align-items: center;
-    border-radius: 4px;
-    height: 62px;
-    width: 250px;
-    padding-left:16px;
-    background-color:${props => getBackgroundColor(props.theme)};
-  .powerIcon{
-    width: 36px;
-    height: 36px;
-  }
-  }
-    .num{
-    margin-left:20px;
-    font-weight: 400;
-    font-size: 13px;
-    div:nth-of-type(1){
-      font-size: 18px;
-      color: #1E50E6;
-      font-weight: 600;
+    justify-content: space-around;
+    .info {
+      display: grid;
+      gap: 16px;
+      color: #606266;
+      .box {
+        display: flex;
+        align-items: center;
+        border-radius: 4px;
+        height: 62px;
+        width: 250px;
+        padding-left: 16px;
+        background-color: ${(props) => getBackgroundColor(props.theme)};
+        .powerIcon {
+          width: 36px;
+          height: 36px;
+        }
+      }
+      .num {
+        margin-left: 20px;
+        font-weight: 400;
+        font-size: 13px;
+        div:nth-of-type(1) {
+          font-size: 18px;
+          color: #1e50e6;
+          font-weight: 600;
+        }
+      }
     }
-    }
-}
-   
-`
+  }
+`;
 
 export const FotterBox = styled.div`
-flex: 1;
+  flex: 1;
   display: grid;
   color: #515151;
-  grid-template-columns: ${props => props.laptop ? "1fr 1fr" : "660px  1fr"} ;
+  grid-template-columns: ${(props) =>
+    props.laptop ? "1fr 1fr" : "660px  1fr"};
   gap: 16px;
   justify-content: flex-end;
- .infoBox3{
-      display:flex;
-      align-items: center;
-      justify-content: space-around;
-      flex-wrap: wrap;
-      .box{
+  .infoBox3 {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-auto-rows: 216px;
+    //  align-items: center;
+    // justify-content: space-around;
+    gap: 16px;
+    flex: 1;
+    .box {
       border-radius: 8px;
-      width: 281px;
+      //  width: 281px;
+      display: flex;
+      flex-direction: column;
       height: 216px;
-      margin-bottom:16px;
-     background-color:${props => getBackgroundColor(props.theme)};
-      .title{
-      border-radius: 8px 8px 0px 0px;
-      width: 281px;
-      height: 36px;
-      line-height: 36px;
-      padding-left: 16px;
-      color: #FFFFFF;
-      }
-      .online{
-      background: #1EC373;
-      }
-      .offline{
-      background: #717D96;
-      }
-      .con{ 
-      padding: 16px;
-      font-weight: 400;
-      font-size: 13px;
-      color: #303133;
-      .top{
-      display:flex;
-      align-items: center;
-      .info{
-      margin-left:16px;
-      display: grid;
-      gap: 5px;
-      .name{
-      font-weight: 400;
-      font-size: 13px;
-      color: #606266;
-      }
-      .status{
-      width: 8px;
-      height: 8px;
-      border-radius: 50%;
-      margin: 0px 5px;
-      display: inline-block;
-      }
-      
-      .online{
-      background: #1EC373;
-      }
-      .offline{
-      background: #717D96;
-      }
-      }
-      }
-      .bottom{
-      margin-top:16px;
-      display:flex;
-      align-items: center;
+      overflow: auto;
+      row-gap: 16px;
       justify-content: space-between;
-       .name{
-      color: #606266;
+      background-color: ${(props) => getBackgroundColor(props.theme)};
+      .title {
+        border-radius: 8px 8px 0px 0px;
+        // width: 281px;
+        height: 36px;
+        line-height: 36px;
+        padding-left: 16px;
+        color: #ffffff;
       }
-      .num{
-      font-weight: 500;
-      font-size: 14px;
-      color: #1E50E6;
+      .online {
+        background: #1ec373;
       }
+      .offline {
+        background: #717d96;
       }
-      }
+      .con {
+        padding: 16px;
+        font-weight: 400;
+        font-size: 13px;
+        color: #303133;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        row-gap: 16px;
+        justify-content: space-between;
+        .top {
+          display: flex;
+          align-items: center;
+          .info {
+            margin-left: 16px;
+            display: grid;
+            gap: 5px;
+            flex: 1;
+            .name {
+              font-weight: 400;
+              font-size: 13px;
+              color: #606266;
+            }
+            .status {
+              width: 8px;
+              height: 8px;
+              border-radius: 50%;
+              margin: 0px 5px;
+              display: inline-block;
+            }
+
+            .online {
+              background: #1ec373;
+            }
+            .offline {
+              background: #717d96;
+            }
+          }
+        }
+        .bottom {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          .name {
+            color: #606266;
+          }
+          .num {
+            font-weight: 500;
+            font-size: 14px;
+            color: #1e50e6;
+          }
+        }
       }
     }
-`
+  }
+`;
 
 export const Header = styled.div`
-width:100%;
-height:32px;
-display:flex;
-flex-direction:row;
-align-items:center;
-justify-content:flex-end;
-.historicalData{
-color: #1E50E6;
-}
+  width: 100%;
+  height: 32px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  .historicalData {
+    color: #1e50e6;
+  }
 `;
