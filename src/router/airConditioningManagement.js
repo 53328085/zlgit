ffa3d29e08c9fc378d010a2357d3e1ddb05b3441
zzy <ybzhangzy3@chint.com>
2025-11-13 08:52:01 +0800

    空调路由修改，碳排统计页面优化

--- src/router/airConditioningManagement.js
+++ src/router/airConditioningManagement.js
@@ -1,14 +1,14 @@
 // 空调控制
-import {lazy} from 'react'
-const airConditioningAutoControl = lazy(() => import("../pages/airConditioningManagement/airConditioningAutoControl"))
-const airConditioningEnergyMonitor= lazy(() => import("../pages/airConditioningManagement/airConditioningEnergyMonitor"))
-const airConditioningOverview = lazy(() => import("../pages/airConditioningManagement/airConditioningOverview"))
-const airConditioningLog = lazy(() => import("../pages/airConditioningManagement/airConditioningLog"))
-const airConditioningManualControl = lazy(() => import("../pages/airConditioningManagement/airConditioningManualControl"))
+import { lazy } from 'react'
+const AirConditioningAutoControl = lazy(() => import("../pages/airConditioningManagement/airConditioningAutoControl"))
+const AirConditioningEnergyMonitor = lazy(() => import("../pages/airConditioningManagement/airConditioningEnergyMonitor"))
+const AirConditioningOverview = lazy(() => import("../pages/airConditioningManagement/airConditioningOverview"))
+const AirConditioningLog = lazy(() => import("../pages/airConditioningManagement/airConditioningLog"))
+const AirConditioningManualControl = lazy(() => import("../pages/airConditioningManagement/airConditioningManualControl"))
 export const airConditioningManagement = {
-    '011901': airConditioningOverview,  //空调总览
-    '011902': airConditioningEnergyMonitor, // 能耗监测
-    '011903': airConditioningManualControl, // 手动控制
-    '011904': airConditioningAutoControl, // 自动控制
-    '011905': airConditioningLog, // 控制日志
+    '011901': AirConditioningOverview,  //空调总览
+    '011902': AirConditioningEnergyMonitor, // 能耗监测
+    '011903': AirConditioningManualControl, // 手动控制
+    '011904': AirConditioningAutoControl, // 自动控制
+    '011905': AirConditioningLog, // 控制日志
 }
