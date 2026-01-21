285a11e037866b71bf24b9eff72f2ca10826bdd2
dingpx <ybdpx@chint.com>
2026-01-21 16:28:27 +0800

    refactor(router): 更新runtimeStorage配置和导入结构
    - 重新组织了runtimeStorage条目，以提高清晰度和一致性。
    - 更新了storage.js中的组件导入，以反映新的结构。
    - 移除了已弃用的条目，并添加了新组件，以增强监控功能。

--- src/router/storage.js
+++ src/router/storage.js
@@ -2,29 +2,23 @@
 import {lazy} from 'react'
 
 const Summary = lazy(() => import("@pages/storage/summary"))
-const AlternatorMonitor = lazy(() => import("@pages/storage/alternatorMonitor"))
 const StorageMonitor = lazy(() => import("@pages/storage/storageMonitor"))
-const StorageControl = lazy(() => import("@pages/storage/storageControl"))
-const ConsumeStatistics = lazy(() => import("@pages/storage/consumeStatistics"))
+const AlternatorMonitor = lazy(() => import("@pages/storage/alternatorMonitor"))
+const BMSMonitor = lazy(() => import("@pages/storage/bmsMonitor"))
+const Environment = lazy(() => import("@pages/storage/environment"))
 const ReportStatistics = lazy(() => import("@pages/storage/reportStatistics"))
 const AlarmMessage = lazy(() => import("@pages/storage/alarmMessage"))
 const OperationLog = lazy(() => import("@pages/storage/operationLog"))
 const StroageReport = lazy(() => import("@pages/storage/StroageReport"))
 
-
-const ChargingOrder = lazy(() => import("@pages/storage/chargingOrder"))
-const Environment = lazy(() => import("@pages/storage/environment"))
-
 export let runtimeStorage = {
-    '011101': Summary,  //储能概述
-    '011102': AlternatorMonitor, // 
-    '011103': StorageMonitor, 
-    '011104': StorageControl, 
-    '011105': ConsumeStatistics, 
-    '011106': ReportStatistics, 
-    '011107': ChargingOrder, 
-    '011108': Environment, 
-    '011109': AlarmMessage, 
-    '011110': OperationLog, 
-    '011111': StroageReport, 
+    '011101': Summary,         //储能总览
+    '011102': StorageMonitor,  //储能柜监控
+    '011103': AlternatorMonitor,  //PCS监控 (复用storageMonitor)
+    '011104': BMSMonitor, //BMS监控
+    '011105': Environment,      //环境监控
+    '011106': ReportStatistics, //收益统计
+    '011107': AlarmMessage,     //告警消息
+    '011108': OperationLog,     //操作日志
+    '011109': StroageReport,    //运行报告
 }
